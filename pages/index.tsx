be1fa621fe7138ca786b46674d15f6ce7d771461
kon-pas <konpas@mail.com>
2022-09-28 18:09:25 +0200

    init: project setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from '@styles/pages/Home.module.css';
+
+import type { NextPage } from 'next';
+
+import Head from 'next/head';
+import Image from 'next/image';
 
 const Home: NextPage = () => {
   return (
