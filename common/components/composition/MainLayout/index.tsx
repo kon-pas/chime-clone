6fa03f7bfc867246bf26c161a8aeeb36ff8feea2
kon-pas <konpas@mail.com>
2022-09-28 18:37:11 +0200

    add: layout

--- common/components/composition/MainLayout/index.tsx
+++ common/components/composition/MainLayout/index.tsx
@@ -1,8 +1,22 @@
 
 
-const MainLayout: React.FC = () => {
+import Header from "@components/composition/Header";
+import Footer from "@components/composition/Footer";
+
+interface MainLayoutProps {
+  children: React.ReactNode;
+}
+
+const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
   return (
     <>
+      <Header />
+
+      <main>
+        {children}
+      </main>
+
+      <Footer />
     </>
   );
 }
