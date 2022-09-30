370559215d24033eb0defb3ff901653fb455a711
kon-pas <konpas@mail.com>
2022-09-30 15:37:43 +0200

    style: `MainLayout`

---
+++ common/components/composition/MainLayout/MainLayout.styled.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const PageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 200vh;
+`
+
+export const HeaderContainer = styled.div`
+  position: sticky;
+  top: 0;
+`
+
+export const FooterContainer = styled.div`
+  margin-top: auto;
+  min-height: 50px;
+`
