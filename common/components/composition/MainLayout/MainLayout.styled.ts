266ebccb65338e3a71540103491bb19bf9929aad
kon-pas <konpas@mail.com>
2022-09-30 19:18:55 +0200

    refactor: add semicolons

--- common/components/composition/MainLayout/MainLayout.styled.ts
+++ common/components/composition/MainLayout/MainLayout.styled.ts
@@ -4,14 +4,14 @@ export const PageContainer = styled.div`
   display: flex;
   flex-direction: column;
   min-height: 100vh;
-`
+`;
 
 export const HeaderContainer = styled.div`
   position: sticky;
   top: 0;
-`
+`;
 
 export const FooterContainer = styled.div`
   margin-top: auto;
   min-height: 50px;
-`
+`;
