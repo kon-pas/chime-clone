8a7bd59a2e9fe97c2d386406f831875288f5bcd3
kon-pas <konpas@mail.com>
2022-09-30 15:38:17 +0200

    add: color constants

---
+++ common/constants/colors.ts
@@ -0,0 +1,21 @@
+import type { HexColor } from '@types';
+
+export const FONT_GRAY_LIGHT: HexColor = '#74758C';
+
+export const FONT_GRAY_MEDIUM: HexColor = '#4A4A4A';
+
+export const FONT_GRAY_DARK: HexColor = '#333333';
+
+export const GRAY_LIGHT: HexColor = '#FAFAFA';
+
+export const LIME_LIGHT: HexColor = '#F0FBF5';
+
+export const LIME_MEDIUM: HexColor = '#2DE28D';
+
+export const LIME_DARK: HexColor = '#1EC677';
+
+export const LIME_MAIN: HexColor = LIME_DARK;
+
+export const WHITE: HexColor = '#FFFFFF';
+
+export const BLACK: HexColor = '#000000';
