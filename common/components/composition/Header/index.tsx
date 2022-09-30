b37d882e61016f78bfd37f0bddd55dda49c42cf8
kon-pas <konpas@mail.com>
2022-09-30 19:18:06 +0200

    add: `Header` content

--- common/components/composition/Header/index.tsx
+++ common/components/composition/Header/index.tsx
@@ -3,7 +3,19 @@ import * as S from './Header.styled';
 const Header: React.FC = () => {
   return (
     <S.Header>
-      header
+      <S.BrandLogo>
+        Chime
+      </S.BrandLogo>
+
+      <S.Navigation>
+        <S.Button>
+          Sign Up
+        </S.Button>
+
+        <div>
+          burger
+        </div>
+      </S.Navigation>
     </S.Header>
   );
 }
