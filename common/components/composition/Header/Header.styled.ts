eea4949465d5db23c35d48f67abd06d149197420
kon-pas <konpas@mail.com>
2022-09-30 19:15:54 +0200

    style: `Header`

--- common/components/composition/Header/Header.styled.ts
+++ common/components/composition/Header/Header.styled.ts
@@ -1,6 +1,28 @@
 import styled from 'styled-components';
 
+import { Button as DefaultButton } from '@components/elements/Button/Button.styled';
+
+import { COLORS } from '@constants';
+
 export const Header = styled.header`
   width: 100%;
-  
+  background-color: white;
+  min-height: 60px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 1rem;
+`;
+
+export const BrandLogo = styled.div`
+
+`;
+
+export const Navigation = styled.nav`
+  display: flex;
+`;
+
+export const Button = styled(DefaultButton)`
+  background-color: ${COLORS.LIME_MEDIUM};
+  border: none;
 `;
