11db1553298967e6f9decbf267284889de598f6a
kon-pas <konpas@mail.com>
2022-09-30 16:04:53 +0200

    fix: page height

--- common/components/composition/MainLayout/MainLayout.styled.ts
+++ common/components/composition/MainLayout/MainLayout.styled.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const PageContainer = styled.div`
   display: flex;
   flex-direction: column;
-  min-height: 200vh;
+  min-height: 100vh;
 `
 
 export const HeaderContainer = styled.div`
