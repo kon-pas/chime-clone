5f3d9b06adb02b68c0fcf1eee95b71c6c5d9c404
kon-pas <konpas@mail.com>
2022-10-01 15:59:31 +0200

    enable external images

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.chime.com',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
