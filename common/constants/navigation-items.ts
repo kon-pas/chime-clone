4e36393fa3e45b8c4b6072d0c95ced671658b6d2
kon-pas <konpas@mail.com>
2022-10-01 16:03:33 +0200

    add: an object with navigation menu items

---
+++ common/constants/navigation-items.ts
@@ -0,0 +1,110 @@
+import type { NavigationItem } from "@interfaces";
+
+export const NAVIGATION: {
+  [key: string]: readonly NavigationItem[];
+} = {
+  "Benefits": [
+    {
+      label: 'No Monthly Fees',
+      isFinished: false
+    },
+    {
+      label: 'Free Debit Card',
+      isFinished: false
+    },
+    {
+      label: 'Fee-Free Overdraft',
+      isFinished: false
+    },
+    {
+      label: 'Build Credit',
+      isFinished: false
+    },
+    {
+      label: 'Get Paid Early',
+      isFinished: false
+    },
+    {
+      label: '60,000+ Fee-Free ATMs',
+      isFinished: false
+    },
+    {
+      label: 'High Yield Savings',
+      isFinished: false
+    },
+    {
+      label: 'Account',
+      isFinished: false
+    },
+    {
+      label: 'Send and Receive Money',
+      isFinished: false
+    },
+    {
+      label: 'Security and Control',
+      isFinished: false
+    }
+  ],
+  "Accounts": [
+    {
+      label: 'Checking Account',
+      isFinished: false
+    },
+    {
+      label: 'Credit Builder',
+      isFinished: false
+    },
+    {
+      label: 'Savings Account',
+      isFinished: false
+    }
+  ],
+  "Who we are": [
+    {
+      label: 'About Us',
+      isFinished: false
+    },
+    {
+      label: 'In the News',
+      isFinished: false
+    },
+    {
+      label: 'Chime Financial, Inc.',
+      isFinished: false
+    },
+    {
+      label: 'In The Community',
+      isFinished: false
+    },
+    {
+      label: 'Careers',
+      isFinished: false
+    }
+  ],
+  "Learn": [
+    {
+      label: 'Blog',
+      isFinished: false
+    },
+    {
+      label: 'Help Center',
+      isFinished: false
+    },
+    {
+      label: 'FAQs',
+      isFinished: false
+    },
+    {
+      label: 'Online Banking',
+      isFinished: false
+    },
+    {
+      label: 'Second Chance Banking',
+      isFinished: false
+    },
+    {
+      label: 'Tax Refund',
+      isFinished: false
+    }
+  ]
+};
