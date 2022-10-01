bfa22ae73b763448b41dcbc88ffe4186e81359b4
kon-pas <konpas@mail.com>
2022-10-01 16:01:20 +0200

    style: `Header`

--- common/components/composition/Header/Header.styled.ts
+++ common/components/composition/Header/Header.styled.ts
@@ -7,22 +7,59 @@ import { COLORS } from '@constants';
 export const Header = styled.header`
   width: 100%;
   background-color: white;
-  min-height: 60px;
+  height: 60px;
   display: flex;
   justify-content: space-between;
   align-items: center;
   padding: 0 1rem;
-`;
+  box-shadow:
+    0 10px 15px -3px rgb(0 0 0 / 0.1),
+    0 4px 6px -4px rgb(0 0 0 / 0.1);
 
-export const BrandLogo = styled.div`
+  h1 {
+    line-height: 1rem;
+  }
 
-`;
+  nav {
+    display: grid;
+    justify-items: end;
+    grid-template-columns: 1fr 0.4fr;
+    height: 55%;
 
-export const Navigation = styled.nav`
-  display: flex;
+    svg {
+      height: 80%;
+      margin: auto 0;
+      color: ${COLORS.LIME_DARK};
+    }
+  }
 `;
 
 export const Button = styled(DefaultButton)`
   background-color: ${COLORS.LIME_MEDIUM};
   border: none;
+  padding: 0.5rem 1.05rem;
+  font-size: 0.95rem;
+  font-weight: 600;
+`;
+
+export const NavigationMenuContainer = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: ${COLORS.WHITE};
+  opacity: 0.98;
+
+  header {
+    width: 100%;
+    height: 55px;
+    border-bottom: solid;
+    border-bottom-width: 2px;
+    border-bottom-color: ${COLORS.LIME_MAIN};
+
+    h1 {
+
+    }
+  }
 `;
