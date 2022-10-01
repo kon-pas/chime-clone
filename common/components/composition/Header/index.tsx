390388a40a6204b2bed8168db2ae1105303ac31b
kon-pas <konpas@mail.com>
2022-10-01 16:05:28 +0200

    add: content to `Header`

--- common/components/composition/Header/index.tsx
+++ common/components/composition/Header/index.tsx
@@ -1,22 +1,50 @@
 import * as S from './Header.styled';
 
+import Image from 'next/image';
+
+import IconSVG from '@components/elements/IconSVG';
+
 const Header: React.FC = () => {
   return (
-    <S.Header>
-      <S.BrandLogo>
-        Chime
-      </S.BrandLogo>
+    <>
+      <S.Header>
+        <h1>
+          <Image
+            src="https://www.chime.com/wp-content/themes/project-sscms-2022-09-29T19-35-23/images/brand/chime-logo.svg"
+            alt="Chime Clone Home Page Brand Logo"
+            width="100px"
+            height="50px"
+          />
+        </h1>
 
-      <S.Navigation>
-        <S.Button>
-          Sign Up
-        </S.Button>
+        <nav>
+          <S.Button>
+            Sign Up
+          </S.Button>
 
-        <div>
-          burger
-        </div>
-      </S.Navigation>
-    </S.Header>
+          <IconSVG>
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+            />
+          </IconSVG>
+        </nav>
+      </S.Header>
+
+      <S.NavigationMenuContainer>
+        <header>
+          <h1>
+            <Image
+              src="https://www.chime.com/wp-content/themes/project-sscms-2022-09-29T19-35-23/images/brand/chime-logo.svg"
+              alt="Chime Clone Home Page Brand Logo"
+              width="100px"
+              height="50px"
+            />
+          </h1>
+        </header>
+      </S.NavigationMenuContainer>
+    </>
   );
 }
 
