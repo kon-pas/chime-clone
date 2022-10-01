da1dc83afeecb957f0f6245bce11790135bf86c4
kon-pas <konpas@mail.com>
2022-10-01 16:06:20 +0200

    feat: component wrapper for all SVGs

---
+++ common/components/elements/IconSVG/IconSVG.styled.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const SVG = styled.svg`
+  width: 100%:
+  height: 100%;
+`;
---
+++ common/components/elements/IconSVG/index.tsx
@@ -0,0 +1,37 @@
+import * as S from './IconSVG.styled';
+
+interface IconSVGProps {
+  children: React.ReactNode;
+  strokeWidth?: number;
+  title?: string;
+  desc?: string;
+  filled?: boolean;
+}
+
+const IconSVG: React.FC<IconSVGProps> = ({
+  children: path,
+  strokeWidth,
+  title,
+  desc,
+  filled = false
+}) => {
+  return (
+    <S.SVG
+      xmlns="http://www.w3.org/2000/svg"
+      fill={filled ? "currentColor" : "none"}
+      viewBox="0 0 24 24"
+      strokeWidth={strokeWidth ?? 1.75}
+      stroke="currentColor"
+      role="img"
+      aria-hidden={title ? false : true}
+      aria-label={`${title ?? null} ${desc ?? null}`}
+      aria-labelledby={`${title ? 'titleID' : null} ${desc ? 'descID' : null}`}
+     > 
+      { title && <title id="titleID">{title}</title> }
+      { desc && <desc id="descID">{desc}</desc> }
+      { path }
+    </S.SVG>
+  )
+}
+
+export default IconSVG;
