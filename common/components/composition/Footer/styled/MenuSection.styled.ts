8deeae5f9665faeae92262f2a453dca1323225e2
kon-pas <konpas@mail.com>
2022-10-03 22:16:42 +0200

    style: `MenuSection` of footer

--- common/components/composition/Footer/styled/MenuSection.styled.ts
+++ common/components/composition/Footer/styled/MenuSection.styled.ts
@@ -1,5 +1,38 @@
 import styled from 'styled-components';
 
-const MenuSection = styled.menu``;
+import { COLORS } from '@constants';
+
+const MenuSection = styled.menu`
+  display: grid;
+  gap: 2rem;
+
+  > li {
+    display: grid;
+    grid-gap: 0.9rem;
+
+    > span {
+      white-space: nowrap;
+      font-size: 1rem;
+      font-weight: 700;
+    }
+
+    > li {
+
+      white-space: nowrap;
+      font-weight: 300;
+      font-size: 0.85rem;
+      user-select: none;
+      cursor: pointer;
+
+      transition-property: color;
+      transition-duration: 0.2s;
+
+      &:hover {
+        color: ${COLORS.LIME_DARK};
+      }
+    }
+  }
+
+`;
 
 export default MenuSection;
