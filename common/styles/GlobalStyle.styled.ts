07043a8c86774af1e3bec71578348490b16564c7
kon-pas <konpas@mail.com>
2022-10-03 07:29:08 +0200

    style: improve global styles

--- common/styles/GlobalStyle.styled.ts
+++ common/styles/GlobalStyle.styled.ts
@@ -1,15 +1,23 @@
 import { createGlobalStyle } from "styled-components";
 
+import { COLORS } from "@constants";
+
 const GlobalStyle = createGlobalStyle`
   * {
     box-sizing: border-box;
     font-family: 'Inter';
+  }
+  
+  #__next {
+    color: ${COLORS.FONT_GRAY_MEDIUM};
   }
 
   html,
   body {
     padding: 0;
     margin: 0;
+    overflow-x: hidden;
+
     /* font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
     Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif; */
   }
@@ -30,6 +38,12 @@ const GlobalStyle = createGlobalStyle`
 
   h1, h2, h3, h4, h5, h6 {
     margin: 0;
+  }
+
+  ul, menu, li {
+    margin: 0;
+    padding: 0;
+    list-style: none;
   }
 `;
 
