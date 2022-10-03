d6f99612ed2418c362f6ba3e0dfd127f8b0d9ae4
kon-pas <konpas@mail.com>
2022-10-03 20:56:17 +0200

    init: `Footer`

--- common/components/composition/Footer/index.tsx
+++ common/components/composition/Footer/index.tsx
@@ -1,10 +1,12 @@
+import * as S from './styled';
 
+import NAVIGATION_ITEMS from '@constants';
 
 const Footer: React.FC = () => {
   return (
-    <>
+    <S.Footer>
       footer
-    </>
+    </S.Footer>
   );
 }
 
