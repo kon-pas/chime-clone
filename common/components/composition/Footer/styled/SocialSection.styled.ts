e070e561dc0a1f7b54f35a1e9a8f6d4f90fd6502
kon-pas <konpas@mail.com>
2022-10-04 22:53:05 +0200

    style: add media queries

--- common/components/composition/Footer/styled/SocialSection.styled.ts
+++ common/components/composition/Footer/styled/SocialSection.styled.ts
@@ -1,5 +1,38 @@
 import styled from 'styled-components';
 
-const SocialSection = styled.div``;
+import { MEDIA } from '@constants';
+
+const SocialSection = styled.div`
+  width: 170px;
+  display: flex;
+	justify-content: space-between;
+  flex-wrap: wrap;
+  gap: 1rem;
+  margin: 0 auto;
+
+  ${MEDIA.MD} {
+    flex-wrap: nowrap;
+    width: auto;
+    justify-content: center;
+  }
+
+  ${MEDIA.LG} {
+    flex-wrap: wrap;
+    width: 170px;
+    justify-content: space-between;
+    margin: 0;
+  }
+
+  > span {
+    cursor: pointer;
+
+    &:hover {
+      transition-property: opacity;
+      transition-duration: 0.2s;
+      transition-timing-function: ease-out;
+      opacity: 0.5;
+    }
+  }
+`;
 
 export default SocialSection;
