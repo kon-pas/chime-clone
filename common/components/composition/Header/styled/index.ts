83577b74366f517b8c32289d69f0dbf34265c30a
kon-pas <konpas@mail.com>
2022-10-04 14:57:39 +0200

    chore: export styles

---
+++ common/components/composition/Header/styled/index.ts
@@ -0,0 +1,11 @@
+export { default  as Header } from './Header.styled';
+
+export { default as Button } from './Button.styled';
+
+export { default as MenuContainer } from './MenuContainer.styled';
+
+export { default as AccountActions } from './AccountActions.styled';
+
+export { default as MenuContainerDesktop } from './MenuContainerDesktop.styled';
+
+export { default as SubmenuContainerDesktop } from './SubmenuContainerDesktop.styled';
