f7da29d3f376fc8ea0e4a26b343193c3351661af
kon-pas <konpas@mail.com>
2022-10-04 22:54:35 +0200

    chore: export constants

--- common/constants/index.ts
+++ common/constants/index.ts
@@ -1,3 +1,7 @@
 export * as COLORS  from './colors';
 
-export { default } from './navigation-items';
+export { default as NAVIGATION_ITEMS } from './navigation-items';
+
+export { default as BREAKPOINTS } from './breakpoints';
+
+export { default as MEDIA } from './media';
