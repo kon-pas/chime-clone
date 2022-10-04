eade2894b867e3576e4ca92289b66e6a1e84b0c3
kon-pas <konpas@mail.com>
2022-10-04 22:49:35 +0200

    refactor: move functionality to separate component

--- common/components/composition/Footer/styled/Footer.styled.ts
+++ common/components/composition/Footer/styled/Footer.styled.ts
@@ -5,16 +5,6 @@ import { COLORS } from '@constants';
 const Footer = styled.footer`
   background-color: ${COLORS.WHITE};
   padding: 4rem 1.2rem 1.5rem 1.2rem;
-
-  hr {
-    height: 1px;
-    border: 0;
-    border-top-width: 1px;
-    border-top-style: solid;
-    border-top-color: ${COLORS.GRAY_MEDIUM};
-    margin: 3rem 0;
-    padding: 0;
-  }
 `;
 
 export default Footer;
