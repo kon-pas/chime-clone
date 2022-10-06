aa9bba4c8fb3e9ee452a4d4c6d3158a4327e2c69
kon-pas <konpas@mail.com>
2022-10-06 22:46:41 +0200

    style: move some `Header` styling to layout

--- common/components/composition/MainLayout/MainLayout.styled.ts
+++ common/components/composition/MainLayout/MainLayout.styled.ts
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
 
+import { COLORS } from '@constants';
+
 export const PageContainer = styled.div`
   display: flex;
   flex-direction: column;
@@ -9,6 +11,9 @@ export const PageContainer = styled.div`
 export const HeaderContainer = styled.div`
   position: sticky;
   top: 0;
+  display: grid;
+  background-color: ${COLORS.WHITE};
+  box-shadow: 0 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1);
 `;
 
 export const FooterContainer = styled.div`
