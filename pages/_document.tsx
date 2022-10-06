b006dba144b616044dd0ee053863bf426766bc0a
kon-pas <konpas@mail.com>
2022-10-06 21:12:50 +0200

    add: favicon

---
+++ pages/_document.tsx
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link rel="icon" href="https://www.chime.com/wp-content/uploads/2018/03/favicon.png" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
