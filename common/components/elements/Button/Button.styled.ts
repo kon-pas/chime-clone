ad5d6bb4f267009b76f5c4dda7d55180d365b182
kon-pas <konpas@mail.com>
2022-10-06 22:26:42 +0200

    fix: border-radius

--- common/components/elements/Button/Button.styled.ts
+++ common/components/elements/Button/Button.styled.ts
@@ -5,7 +5,7 @@ import { COLORS } from "@constants";
 export const Button = styled.button`
   border-color: ${COLORS.BLACK}
   border-width: 1px;
-  border-radius: 22px;
+  border-radius: 10rem;
   text-align: center;
   white-space: nowrap;
 `;
