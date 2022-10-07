92d9e64af4842578e1b44bea7d3e39504280fdf1
kon-pas <konpas@mail.com>
2022-10-07 13:59:04 +0200

    feat move z-index values to separate file

--- common/constants/index.ts
+++ common/constants/index.ts
@@ -5,3 +5,5 @@ export { default as NAVIGATION_ITEMS } from './navigation-items';
 export { default as BREAKPOINTS } from './breakpoints';
 
 export { default as MEDIA } from './media';
+
+export { default as Z_INDEX } from './z-index';
---
+++ common/constants/z-index.ts
@@ -0,0 +1,5 @@
+const Z_INDEX: { [key: string]: number } = {
+  HEADER: 99
+}
+
+export default Z_INDEX;
