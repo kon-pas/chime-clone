8ca49fff26e2a9f8a82380c545bc3731089d32b7
kon-pas <konpas@mail.com>
2022-10-08 21:43:33 +0200

    style(`DualArtice`): improve RWD

--- common/components/elements/DualArticle/styled/Container.styled.ts
+++ common/components/elements/DualArticle/styled/Container.styled.ts
@@ -1,9 +1,15 @@
 import styled from 'styled-components';
 
+import { MEDIA } from '@constants';
+
 import ContentContainer from '@components/elements/ContentContainer';
 
 const Container = styled(ContentContainer)`
-
+  ${MEDIA.MD} {
+    display: flex;
+    flex-direction: row;
+    gap: 5rem;
+  }
 `;
 
 export default Container;
