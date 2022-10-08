e68dfcacf32abc722e321a9ef47defc36a82cfef
kon-pas <konpas@mail.com>
2022-10-08 21:10:12 +0200

    style(`ContentContainer`): nicer looking padding

--- common/components/elements/ContentContainer/ContentContainer.styled.ts
+++ common/components/elements/ContentContainer/ContentContainer.styled.ts
@@ -3,10 +3,12 @@ import styled from "styled-components";
 import { MEDIA, BREAKPOINTS } from "@constants";
 
 const ContentContainer = styled.div`
+  padding: 0 1.4rem;
 
   ${ MEDIA.MD } {
     width: ${BREAKPOINTS.MD}px;
     margin: 0 auto;
+    padding: 0;
   }
 
   ${ MEDIA.LG } {
