6c1759ec0ea1d33f8fe7a9989e95f52907188ef0
kon-pas <konpas@mail.com>
2022-10-08 21:12:46 +0200

    add(home): `DualArticle`

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,10 @@ import type { NextPage } from 'next';
 
 import Head from 'next/head';
 
+import Image from 'next/image';
+
+import DualArticle from '@components/elements/DualArticle';
+
 const Home: NextPage = () => {
   return (
     <div>
@@ -10,7 +14,24 @@ const Home: NextPage = () => {
         <meta name="description" content="Chime clone home page" />
       </Head>
 
-      blah
+      <DualArticle.Container>
+        <DualArticle.Header>
+          <h1>Overdraft fee-free with SpotMe</h1>
+          <p>
+            We&apos;ll spot you up to $200 on debit card purchases with no overdraft fees. Eligibility requirements apply.<sup>1</sup>
+          </p>
+        </DualArticle.Header>
+
+        <DualArticle.ImageWrapper>
+          <Image
+            src="https://www.chime.com/wp-content/uploads/2021/03/spot-me-1.png"
+            alt="Overdraft fee-free with SpotMe"
+            layout="responsive"
+            width="792"
+            height="526"
+          />
+        </DualArticle.ImageWrapper>
+      </DualArticle.Container>
     </div>
   )
 }
