59cca0ebf2248e962fbf2422466fa4fe6014185c
kon-pas <konpas@mail.com>
2022-10-08 21:11:21 +0200

    chore(`DualArticle`): export components

---
+++ common/components/elements/DualArticle/styled/index.ts
@@ -0,0 +1,5 @@
+export { default as Container } from './Container.styled';
+
+export { default as Header } from './Header.styled';
+
+export { default as ImageWrapper } from './ImageWrapper.styled';
