ab100152b8451fe8785e489f3adb2ec94304838e
kon-pas <konpas@mail.com>
2022-10-09 04:17:59 +0200

    refactor(`Header`): better component naming

--- common/components/composition/Header/styled/index.ts
+++ common/components/composition/Header/styled/index.ts
@@ -2,12 +2,14 @@ export { default  as Header } from './Header.styled';
 
 export { default as Button } from './Button.styled';
 
-export { default as Menu } from './Menu.styled';
+export { default as SlidePageContainer } from './SlidePageContainer.styled';
+
+export { default as MenuContainer } from './MenuContainer.styled';
 
 export { default as AccountActions } from './AccountActions.styled';
 
-export { default as MenuDesktop } from './MenuDesktop.styled';
+export { default as MenuContainerDesktop } from './MenuContainerDesktop.styled';
 
-export { default as SubmenuDesktop } from './SubmenuDesktop.styled';
+export { default as SubmenuContainerDesktop } from './SubmenuContainerDesktop.styled';
 
 export { default as Span } from './Span.styled';
