d5fc2d214d2821e3e25140a754436d202e08988b
kon-pas <konpas@mail.com>
2022-10-09 02:32:40 +0200

    chore(`Footer`): clean up redundant attributes

--- common/components/composition/Footer/styled/SocialSection.styled.ts
+++ common/components/composition/Footer/styled/SocialSection.styled.ts
@@ -27,25 +27,6 @@ const SocialSection = styled.div`
   > a {
     height: 40px;
   }
-
-  > div {
-    height: 40px;
-    cursor: pointer;
-    display: flex;
-    align-items: center;
-
-    > a {
-      display: inline-block;
-      height: 32px;
-    }
-
-    &:hover {
-      transition-property: opacity;
-      transition-duration: 0.2s;
-      transition-timing-function: ease-out;
-      opacity: 0.5;
-    }
-  }
 `;
 
 export default SocialSection;
