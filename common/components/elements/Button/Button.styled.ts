a1d44690b38010fc2d335d626a935d3841061b69
kon-pas <konpas@mail.com>
2022-10-09 02:09:32 +0200

    style(`Button`): add default styling

--- common/components/elements/Button/Button.styled.ts
+++ common/components/elements/Button/Button.styled.ts
@@ -2,10 +2,26 @@ import styled from "styled-components";
 
 import { COLORS } from "@constants";
 
-export const Button = styled.button`
-  border-color: ${COLORS.BLACK}
-  border-width: 1px;
+const Button = styled.button`
+  border: 1px solid ${COLORS.BLACK};
   border-radius: 10rem;
   text-align: center;
   white-space: nowrap;
+  font-size: 1rem;
+  background: none;
+  padding: 0.7rem 1.3rem;
+  transition: all 0.2s linear;
+
+  &:hover {
+    background-color: ${COLORS.BLACK};
+    color: ${COLORS.WHITE};
+    transition: all 0.2s linear;
+  }
+
+  svg {
+    margin-left: 3px;
+    width: 10px;
+  }
 `;
+
+export default Button;
