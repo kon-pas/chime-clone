73e7922446958fe794de83df475a901adb34fede
kon-pas <konpas@mail.com>
2022-10-12 00:44:58 +0200

    wip(home): fix content

--- pages/index.tsx
+++ pages/index.tsx
@@ -75,95 +75,37 @@ const Home: NextPage = () => {
         img={{
           src: "https://www.chime.com/wp-content/uploads/2020/03/paid-early.png",
           alt: "Overdraft fee-free with SpotMe",
-          width: 713,
-          height: 202,
+          width: 826,
+          height: 670,
         }}
       >
-        <h1>Get paid early</h1>
+        <h1>A new way to build credit</h1>
         <p>
-          Set up direct deposit and get your paycheck up to 2 days earlier than
-          some of your co-workers!<sup>2</sup>
+          Help increase your FICO Score<sup>®</sup> by an average of 30 points
+          with our new secured credit card.<sup>4</sup> No interest, no annual
+          fees, no credit check to apply. Eligibility requirements apply.
+          <sup>5</sup>
         </p>
         <LearnMoreButton />
       </DualArticle>
-      {/* <DualArticle.Container imageFloat="left">
-        <DualArticle.Header>
-          <h1>A new way to build credit</h1>
-          <p>
-            Help increase your FICO Score<sup>®</sup> by an average of 30 points
-            with our new secured credit card.<sup>4</sup> No interest, no annual
-            fees, no credit check to apply. Eligibility requirements apply.
-            <sup>5</sup>
-          </p>
-          <Button>
-            Learn more
-            <IconSVG strokeWidth={3}>
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M8.25 4.5l7.5 7.5-7.5 7.5"
-              />
-            </IconSVG>
-          </Button>
-        </DualArticle.Header>
-
-        <DualArticle.ImageWrapper>
-          <Image
-            src="https://www.chime.com/wp-content/uploads/2021/03/CreditBuilder.png"
-            alt="Overdraft fee-free with SpotMe"
-            layout="responsive"
-            width="826"
-            height="670"
-          />
-        </DualArticle.ImageWrapper>
-      </DualArticle.Container> */}
 
       {/* 5 */}
       <DualArticle
         img={{
-          src: "https://www.chime.com/wp-content/uploads/2020/03/paid-early.png",
+          src: "https://www.chime.com/wp-content/uploads/2021/07/pay-anyone-desktop.png",
           alt: "Overdraft fee-free with SpotMe",
-          width: 713,
-          height: 202,
+          width: 813,
+          height: 916,
         }}
       >
-        <h1>Get paid early</h1>
+        <h1>Pay anyone the fast, fee-free way</h1>
         <p>
-          Set up direct deposit and get your paycheck up to 2 days earlier than
-          some of your co-workers!<sup>2</sup>
+          Pay friends and family fee-free, no matter what bank account they
+          use. They can cash out instantly<sup>6</sup> and securely, no
+          sign-up needed.
         </p>
         <LearnMoreButton />
       </DualArticle>
-      {/* <DualArticle.Container imageFloat="left">
-        <DualArticle.Header>
-          <h1>Pay anyone the fast, fee-free way</h1>
-          <p>
-            Pay friends and family fee-free, no matter what bank account they
-            use. They can cash out instantly<sup>6</sup> and securely, no
-            sign-up needed.
-          </p>
-          <Button>
-            Learn more
-            <IconSVG strokeWidth={3}>
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M8.25 4.5l7.5 7.5-7.5 7.5"
-              />
-            </IconSVG>
-          </Button>
-        </DualArticle.Header>
-
-        <DualArticle.ImageWrapper>
-          <Image
-            src="https://www.chime.com/wp-content/uploads/2021/07/pay-anyone-desktop.png"
-            alt="Overdraft fee-free with SpotMe"
-            layout="responsive"
-            width="831"
-            height="916"
-          />
-        </DualArticle.ImageWrapper>
-      </DualArticle.Container> */}
 
       {/* 6 */}
       <DualArticle
