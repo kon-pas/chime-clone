33629e6d9bb8f955048d05f42be98ee01144f2b2
kon-pas <konpas@mail.com>
2022-10-13 01:02:18 +0200

    fix: timing function

--- common/utils/resize-transition.ts
+++ common/utils/resize-transition.ts
@@ -1,6 +1,6 @@
 const resizeTransition = (...properties: string[]): string =>
   `transition: ${properties.map((property, idx, properties) =>
-    `${property} 0.5s linear${idx === properties.length - 1 ? ";" : ","}`
+    `${property} 0.5s ease-out${idx === properties.length - 1 ? ";" : ","}`
   )}`;
 
 export default resizeTransition;
