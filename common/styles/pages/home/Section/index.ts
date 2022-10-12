16cf703f65431e7471dd9b6f96c872d4b13b53ef
kon-pas <konpas@mail.com>
2022-10-12 05:33:35 +0200

    chore(home): export `Section` styled components

---
+++ common/styles/pages/home/Section/index.ts
@@ -0,0 +1,11 @@
+export { default as Wrapper } from "./Wrapper.styled";
+
+export { default as Container } from "./Container.styled";
+
+export { default as ImageWrapper } from "./ImageWrapper.styled";
+
+export { default as TextWrapper } from "./TextWrapper.styled";
+
+export { default as Title } from "./Title.styled";
+
+export { default as Paragraph } from "./Paragraph.styled";
