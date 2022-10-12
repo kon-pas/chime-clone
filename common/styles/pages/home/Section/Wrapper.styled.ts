2d264cb28bfa8909fd23e78944a77a17c9977f4d
kon-pas <konpas@mail.com>
2022-10-12 05:35:08 +0200

    add(home): `Wrapper`

---
+++ common/styles/pages/home/Section/Wrapper.styled.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+import { COLORS } from "@constants";
+
+const Wrapper = styled.section`
+  width: 100%;
+  padding: 5rem 0;
+  background-color: ${COLORS.GRAY_LIGHT};
+`;
+
+export default Wrapper;
