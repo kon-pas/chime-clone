507f279a5da738bb6826892e2551878de49a262f
kon-pas <konpas@mail.com>
2022-10-13 05:38:26 +0200

    add(config): redirect `/home` to `/`

--- next.config.js
+++ next.config.js
@@ -10,6 +10,15 @@ const nextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/home',
+        destination: '/',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
