04e8ac3b0a69f8b43d4ff3316fc52fd3bbb18dbd
kon-pas <konpas@mail.com>
2022-10-13 12:14:34 +0200

    style: `LoadingPage`

---
+++ common/components/composition/LoadingPage/LoadingPage.styled.ts
@@ -0,0 +1,42 @@
+import styled, { keyframes } from "styled-components";
+
+import { COLORS, Z_INDEX } from "@constants";
+
+const spin = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+export const LoadingPageWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100vh;
+  width: 100%;
+  background-color: ${COLORS.WHITE};
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  z-index: ${Z_INDEX.LOADING_PAGE};
+
+  svg {
+    width: 2rem;
+    height: 2rem;
+
+    animation: ${spin} 2s linear infinite;
+  }
+`;
+
+export const AnimationWrapper = styled.div`
+
+`;
+
+
+
