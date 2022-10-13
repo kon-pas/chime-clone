44d0fb2d9a455376af560f2989164914b74b9721
kon-pas <konpas@mail.com>
2022-10-13 05:41:29 +0200

    style(`Footer`): add screen resizing transition

--- common/components/composition/Footer/styled/Footer.styled.ts
+++ common/components/composition/Footer/styled/Footer.styled.ts
@@ -1,12 +1,20 @@
 import styled from "styled-components";
 
-import { COLORS, MEDIA, BREAKPOINTS } from "@constants";
+import { COLORS, MEDIA } from "@constants";
+
+import { resizeTransition } from "@utils";
 
 const Footer = styled.footer`
   background-color: ${COLORS.WHITE};
   padding: 4rem 0 1.5rem 0;
   width: 100%;
   margin: 0 auto;
+
+  ${resizeTransition('padding')}
+  
+  ${MEDIA.MD} {
+    padding: 4rem 0 5rem 0;
+  }
 `;
 
 export default Footer;
