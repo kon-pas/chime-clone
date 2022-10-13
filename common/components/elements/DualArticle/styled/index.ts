1d852922bd6e565a705e3702491d8e26604cbb40
kon-pas <konpas@mail.com>
2022-10-13 05:40:11 +0200

    chore(elements): `DualArticle` to `DualSection`

--- common/components/elements/DualArticle/styled/index.ts
+++
@@ -1,7 +0,0 @@
-export { default as Wrapper } from "./Wrapper.styled";
-
-export { default as Container } from "./Container.styled";
-
-export { default as Header } from "./Header.styled";
-
-export { default as ImageWrapper } from "./ImageWrapper.styled";
