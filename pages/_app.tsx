30b537771c404f0d43d4bec887dbfa971d1b5098
kon-pas <konpas@mail.com>
2022-10-13 12:13:53 +0200

    add: loading page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,18 +2,42 @@ import "@styles/fonts.css";
 
 import GlobalStyle from "@styles/GlobalStyle.styled";
 
+import { useEffect, useState } from "react";
+
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
 
 import MainLayout from "@components/composition/MainLayout";
+import LoadingPage from "@components/composition/LoadingPage";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const { events } = useRouter();
+
+  useEffect(() => {
+    const hidePageContent = ()  => setIsLoading(true);
+    const showPageContent = ()  => setIsLoading(false);
+
+    events.on("routeChangeStart", hidePageContent);
+    events.on("routeChangeComplete", showPageContent);
+    events.on("routeChangeError", showPageContent);
+
+    return () => {
+      events.off("routeChangeStart", hidePageContent);
+      events.off("routeChangeComplete", showPageContent);
+      events.off("routeChangeError", showPageContent);
+    }
+  })
+
+  return isLoading
+    ? <LoadingPage />
+    : (
     <MainLayout>
       <GlobalStyle />
-
       <Component {...pageProps} />
     </MainLayout>
-  );
+    );
 }
 
 export default MyApp;
