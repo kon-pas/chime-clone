db935fc5cf6a039225143ff01357406a8e790444
kon-pas <konpas@mail.com>
2022-10-13 08:21:26 +0200

    add(`DualSection`): optional image fixed width

--- common/components/elements/DualSection/styled/ImageWrapper.styled.ts
+++ common/components/elements/DualSection/styled/ImageWrapper.styled.ts
@@ -4,20 +4,27 @@ import { MEDIA } from "@constants";
 
 import { resizeTransition } from "@utils";
 
-const ImageWrapper = styled.div`
+interface ImageWrapperProps {
+  sm?: number;
+  md?: number;
+  lg?: number;
+}
+
+const ImageWrapper = styled.div<ImageWrapperProps>`
   margin: 1.25rem auto 0 auto;
-  max-width: 440px;
+  width: 100%;
+  max-width: ${props => props.sm ?? '440px'};
 
   ${resizeTransition("all")}
 
   ${MEDIA.MD} {
-    width: 365px;
+    max-width: ${props => props.md ?? '365px'};
     margin: auto 0;
   }
 
   ${MEDIA.LG} {
+    max-width: ${props => props.lg ?? '365px'};
     margin: auto;
-    width: 100%;
   }
 `;
 
