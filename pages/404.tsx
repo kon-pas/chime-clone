5b5caf8fc3e1b06ad1e088ef5b7717dfbd19fafe
kon-pas <konpas@mail.com>
2022-10-15 02:40:28 +0200

    init: `Page404`

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,11 +1,36 @@
+import * as Styled from "@styles/pages/404";
+
 import type { NextPage } from "next";
+import Image from "next/image";
+
+import ContentContainer from "@components/elements/ContentContainer";
 
 const Page404: NextPage = () => {
-  return(
-    <>
-      Oops!
-    </>
-  )
-}
+  return (
+    <Styled.Container>
+      <ContentContainer>
+        <Styled.TextContainer>
+          <Styled.Heading>
+            Uh-oh. The page you&apos;re looking for isn&apos;t here!
+          </Styled.Heading>
+
+          <Styled.Button>
+            Back to Homepage
+          </Styled.Button>
+        </Styled.TextContainer>
+
+        <Styled.ImageWrapper>
+          <Image
+            src="https://www.chime.com/wp-content/themes/project-sscms-2022-10-05T17-28-22/images/404.png"
+            alt="404 Robot"
+            layout="responsive"
+            width={654}
+            height={564}
+          />
+        </Styled.ImageWrapper>
+      </ContentContainer>
+    </Styled.Container>
+  );
+};
 
 export default Page404;
