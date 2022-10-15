fe1d70ef00aa294b5b6f9e74a8619421789929e6
kon-pas <konpas@mail.com>
2022-10-15 02:43:08 +0200

    chore(`Page404`): export styled components

---
+++ common/styles/pages/404/index.ts
@@ -0,0 +1,9 @@
+export { default as Button } from "./Button.styled";
+
+export { default as Container } from "./Container.styled";
+
+export { default as Heading } from "./Heading.styled";
+
+export { default as ImageWrapper } from "./ImageWrapper.styled";
+
+export { default as TextContainer } from "./TextContainer.styled";
