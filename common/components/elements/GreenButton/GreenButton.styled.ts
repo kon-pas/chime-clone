c8917c3347fcf511f23a89a5e46e85009dee8927
kon-pas <konpas@mail.com>
2022-10-15 04:26:42 +0200

    init: `GreenButton`, an extended `Button`

---
+++ common/components/elements/GreenButton/GreenButton.styled.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+import { COLORS } from "@constants";
+
+import Button from "@components/elements/Button";
+
+const GreenButton = styled(Button)`
+  background-color: ${COLORS.LIME_MEDIUM};
+  border: none;
+  font-weight: 600;
+
+  &:hover {
+    color: ${COLORS.BLACK};
+    background-color: ${COLORS.LIME_DARK};
+  }
+`;
+
+export default GreenButton;
