616283f7e54f45dabee950bdcb3c8aa0828a0d4f
kon-pas <konpas@mail.com>
2022-10-15 02:41:33 +0200

    style(`Page404`): `Container`

---
+++ common/styles/pages/404/Container.styled.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+import { COLORS } from "@constants";
+
+const Container = styled.div`
+  padding-top: 3rem;
+  background: linear-gradient(to top, ${COLORS.CYAN_LIGTH}, ${COLORS.WHITE});
+`;
+
+export default Container;
