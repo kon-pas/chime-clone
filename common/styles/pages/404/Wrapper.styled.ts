1d17f67d24e552ce8f2331fca198814cba6f48a7
kon-pas <konpas@mail.com>
2022-10-15 03:51:01 +0200

    init(`Page404`): `Wrapper`

---
+++ common/styles/pages/404/Wrapper.styled.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+import { COLORS, MEDIA } from "@constants";
+
+const Wrapper = styled.div`
+  padding-top: 3rem;
+  background: linear-gradient(to top, ${COLORS.CYAN_LIGTH}, ${COLORS.WHITE});
+`;
+
+export default Wrapper;
--- common/styles/pages/404/index.ts
+++ common/styles/pages/404/index.ts
@@ -1,5 +1,7 @@
 export { default as Button } from "./Button.styled";
 
+export { default as Wrapper } from "./Wrapper.styled";
+
 export { default as Container } from "./Container.styled";
 
 export { default as Heading } from "./Heading.styled";
