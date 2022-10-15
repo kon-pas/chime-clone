1bcd800951b6577488523a873ca275483bf61c8c
kon-pas <konpas@mail.com>
2022-10-15 04:29:03 +0200

    feat(`Button`): convert to pure `styled-component`

--- common/components/elements/Button/index.tsx
+++ common/components/elements/Button/index.tsx
@@ -1,11 +1,1 @@
-import StyledButton from "./Button.styled";
-
-interface ButtonProps {
-  children: React.ReactNode;
-}
-
-const Button: React.FC<ButtonProps> = ({ children }) => (
-  <StyledButton>{children}</StyledButton>
-);
-
-export default Button;
+export { default } from "./Button.styled";
