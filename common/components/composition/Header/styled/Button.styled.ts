a17d83d93ecd5bfb3a7c8d19ee9849939aacf358
kon-pas <konpas@mail.com>
2022-10-15 04:25:19 +0200

    chore(`Header`): extend from `GreenButton`

--- common/components/composition/Header/styled/Button.styled.ts
+++ common/components/composition/Header/styled/Button.styled.ts
@@ -4,21 +4,11 @@ import { COLORS, MEDIA } from "@constants";
 
 import { resizeTransition } from "@utils";
 
-import DefaultButton from "@components/elements/Button/Button.styled";
+import GreenButton from "@components/elements/GreenButton";
 
-const Button = styled(DefaultButton)`
-  background-color: ${COLORS.LIME_MEDIUM};
-  border: none;
+const Button = styled(GreenButton)`
   padding: 0.5rem 1.05rem;
   margin-left: 0.5rem;
-  // margin-right: 20px;
-  font-size: 0.95rem;
-  font-weight: 600;
-
-  &:hover {
-    color: ${COLORS.BLACK};
-    background-color: ${COLORS.LIME_DARK};
-  }
 
   ${MEDIA.MD} {
     padding: 0.75rem 1.4rem;
