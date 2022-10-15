61c8695d5eaf42c38336a86b8da114c30ce0d9b6
kon-pas <konpas@mail.com>
2022-10-15 02:40:42 +0200

    refactor(`/home`): convert wrapper to container

--- common/styles/pages/home/Section/index.ts
+++ common/styles/pages/home/Section/index.ts
@@ -4,6 +4,6 @@ export { default as Container } from "./Container.styled";
 
 export { default as ImageWrapper } from "./ImageWrapper.styled";
 
-export { default as TextWrapper } from "./TextWrapper.styled";
+export { default as TextContainer } from "./TextContainer.styled";
 
 export { default as Title } from "./Title.styled";
