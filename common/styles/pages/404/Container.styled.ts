b5c04fc6d1864c47bb9d786ac5f2bb9c9a94b042
kon-pas <konpas@mail.com>
2022-10-15 03:50:45 +0200

    style(`Page404`): improve RWD

--- common/styles/pages/404/Container.styled.ts
+++ common/styles/pages/404/Container.styled.ts
@@ -1,10 +1,13 @@
 import styled from "styled-components";
 
-import { COLORS } from "@constants";
+import { MEDIA } from "@constants";
 
-const Container = styled.div`
-  padding-top: 3rem;
-  background: linear-gradient(to top, ${COLORS.CYAN_LIGTH}, ${COLORS.WHITE});
+import ContentContainer from "@components/elements/ContentContainer/ContentContainer.styled";
+
+const Container = styled(ContentContainer)`
+  ${MEDIA.MD} {
+    display: flex;
+  }
 `;
 
 export default Container;
