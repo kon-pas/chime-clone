9660d6c0b9aa962c8b0169470de23df13e73da6f
kon-pas <konpas@mail.com>
2022-10-15 02:41:26 +0200

    style(`Page404`): `Button`

---
+++ common/styles/pages/404/Button.styled.ts
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+import { COLORS, MEDIA } from "@constants";
+
+import { resizeTransition } from "@utils";
+
+import DefaultButton from "@components/elements/Button/Button.styled";
+
+const Button = styled(DefaultButton)`
+  background-color: ${COLORS.LIME_MEDIUM};
+  border: none;
+  font-weight: 600;
+  padding-top: 1rem;
+  padding-bottom: 1rem;
+
+  // ${resizeTransition('padding-top', 'padding-bottom')}
+
+  ${MEDIA.MD} {
+
+  }
+`;
+
+export default Button;
