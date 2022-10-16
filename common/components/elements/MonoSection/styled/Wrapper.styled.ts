d71fba28f8f76be548741b1cb0c6334a3f465d5a
kon-pas <konpas@mail.com>
2022-10-16 19:18:51 +0200

    init(`MonoSection`): `Wrapper`

---
+++ common/components/elements/MonoSection/styled/Wrapper.styled.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+import type { HexColor } from "@types";
+
+import { COLORS, MEDIA } from "@constants";
+
+interface WrapperProps {
+  backgroundColor?: HexColor;
+}
+
+const Wrapper = styled.div<WrapperProps>`
+  width: 100%;
+  background-color: ${props => props.backgroundColor ?? COLORS.WHITE};
+  padding: 3rem 0;
+
+  ${MEDIA} {
+    padding: 6rem 0;
+  }
+`;
+
+export default Wrapper;
