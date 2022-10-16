f5ce70cfad981f327e5caf81f5a57944adbb6e75
kon-pas <konpas@mail.com>
2022-10-16 23:42:50 +0200

    init: `BreadcrumbTrial`

---
+++ common/components/elements/BreadcrumbTrail/index.tsx
@@ -0,0 +1,17 @@
+import * as Styled from './BreadcrumbTrail.styled';
+
+import { useRouter } from "next/router";
+
+import ContentContainer from "@components/elements/ContentContainer";
+
+const BreadcrumbTrial: React.FC = () => {
+
+  return (
+    <Styled.BreadcrumbTrial>
+      <ContentContainer>
+      </ContentContainer>
+    </Styled.BreadcrumbTrial>
+  );
+}
+
+export default BreadcrumbTrial;
