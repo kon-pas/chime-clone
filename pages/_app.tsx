ffd29fe78b9e01da5c51e2a88822bd912ea5843f
kon-pas <konpas@mail.com>
2022-10-17 08:48:05 +0200

    refactor: move loading logic to `Loader`

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,43 +1,16 @@
-// import "@styles/fonts.css";
-
 import GlobalStyle from "@styles/GlobalStyle.styled";
 
-import { useEffect, useState } from "react";
-
 import type { AppProps } from "next/app";
-import { useRouter } from "next/router";
 
 import MainLayout from "@components/composition/MainLayout";
-import LoadingPage from "@components/composition/LoadingPage";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-
-  const { events } = useRouter();
-
-  useEffect(() => {
-    const hidePageContent = () => setIsLoading(true);
-    const showPageContent = () => setIsLoading(false);
-
-    events.on("routeChangeStart", hidePageContent);
-    events.on("routeChangeComplete", showPageContent);
-    events.on("routeChangeError", showPageContent);
-
-    return () => {
-      events.off("routeChangeStart", hidePageContent);
-      events.off("routeChangeComplete", showPageContent);
-      events.off("routeChangeError", showPageContent);
-    };
-  });
-
-  return isLoading ? (
-    <LoadingPage />
-  ) : (
+  return (
     <MainLayout>
-      <GlobalStyle />
+      <GlobalStyle /> 
       <Component {...pageProps} />
     </MainLayout>
-  );
+    );
 }
 
 export default MyApp;
