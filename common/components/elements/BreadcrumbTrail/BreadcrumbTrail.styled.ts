310e1cd66633fc9495b924b801e8590dd4a3e7ac
kon-pas <konpas@mail.com>
2022-10-18 05:21:24 +0200

    wip(`BreadcrumbTrail`): styles dont apply

--- common/components/elements/BreadcrumbTrail/BreadcrumbTrail.styled.ts
+++ common/components/elements/BreadcrumbTrail/BreadcrumbTrail.styled.ts
@@ -1,5 +1,32 @@
-import styled from "styled-components";
+// import styled from "styled-components";
 
-export const BreadcrumbTrial = styled.div`
+// import { COLORS } from "@constants";
 
-`;
+// export const List = styled.menu`
+//   display: flex;
+//   height: 50px;
+// `;
+
+// export const Item = styled.li`
+
+// `;
+
+// export const ArrowWrapper = styled.div`
+//   overflow: hidden;
+//   height: 10px;
+
+//   > svg {
+//     width: 30px;
+//     color: ${COLORS.LIME_DARK};
+//     cursor: pointer;
+//     display: block;
+//   }
+// `;
+
+// export const LabelWrapper = styled.div`
+//   cursor: pointer;
+
+//   &:hover {
+//     color: ${COLORS.LIME_DARK};
+//   }
+// `;
