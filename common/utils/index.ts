e3681c634cc3ca5039fd6aff9ac07b195f8fee43
kon-pas <konpas@mail.com>
2022-10-19 19:06:34 +0200

    add(utils): `getMedia` export

--- common/utils/index.ts
+++ common/utils/index.ts
@@ -1,3 +1,5 @@
 export { default as resizeTransition } from "./use-resize-transition";
 
 export { default as keysToLowercase } from "./keys-to-lowercase";
+
+export { default as getMedia } from "./get-media";
