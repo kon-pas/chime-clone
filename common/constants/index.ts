1bd951d3b6ae1d10aa48d389466b13252c9834ff
kon-pas <konpas@mail.com>
2022-10-19 19:05:05 +0200

    chore(constants): deprecate `MEDIA`

--- common/constants/index.ts
+++ common/constants/index.ts
@@ -4,6 +4,6 @@ export { default as NAVIGATION_ITEMS } from './navigation-items';
 
 export { default as BREAKPOINTS } from './breakpoints';
 
-export { default as MEDIA } from './media';
+// export { default as MEDIA } from './media';
 
 export { default as Z_INDEX } from './z-index';
