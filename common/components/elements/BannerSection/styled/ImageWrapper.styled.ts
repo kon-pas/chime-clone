fe8979738e683d4962ce7bac4fb533f0790d137c
kon-pas <konpas@mail.com>
2022-10-19 15:54:40 +0200

    style(`BannerSection`): `ImageWrapper`

--- common/components/elements/BannerSection/styled/ImageWrapper.styled.ts
+++ common/components/elements/BannerSection/styled/ImageWrapper.styled.ts
@@ -1,12 +1,28 @@
 import styled from "styled-components";
 
-import { MEDIA, BREAKPOINTS } from "@constants";
+import { MEDIA } from "@constants";
 
-const ImageWrapper = styled.div`
-  margin: 3rem auto 0 auto;
+interface ImageWrapper {
+  align?: "center" | "bottom";
+  sm?: number;
+  md?: number;
+  lg?: number;
+}
+
+const ImageWrapper = styled.div<ImageWrapper>`
+  margin-top: 3rem;
+  max-width: ${props => props.sm ?? "100%"};
 
   ${MEDIA.MD} {
     width: 50%;
+    display: grid;
+    margin-top: 0;
+    align-content: ${props => props.align === "bottom" ? "flex-end" : "center"};
+    max-width: ${props => props.md ?? "50%"};
+  }
+
+  ${MEDIA.LG} {
+    max-width: ${props => props.lg ?? "50%"};
   }
 `;
 
