95e388d0cb767b44c91152734d31a35eb35ce5a5
kon-pas <konpas@mail.com>
2022-10-19 12:46:57 +0200

    init(`BannerSection`): `Wrapper`

--- common/components/elements/BannerSection/styled/Wrapper.styled.ts
+++ common/components/elements/BannerSection/styled/Wrapper.styled.ts
@@ -1,5 +1,17 @@
 import styled from "styled-components";
 
-const Wrapper = styled.div``;
+import type { HexColor } from "@types";
+
+import { COLORS } from "@constants";
+
+interface WrapperProps {
+  backgroundColor?: HexColor;
+}
+
+const Wrapper = styled.div<WrapperProps>`
+  background-color: ${props => props.backgroundColor ?? COLORS.WHITE};
+  width: 100%;
+  padding: 1rem 0 4rem 0;
+`;
 
 export default Wrapper;
