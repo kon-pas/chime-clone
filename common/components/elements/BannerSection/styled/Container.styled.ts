9b19d3e9f857ecdf7a3f24cba87cb36eb4e1cb65
kon-pas <konpas@mail.com>
2022-10-19 14:28:43 +0200

    style(`BannerSection`): improve RWD

--- common/components/elements/BannerSection/styled/Container.styled.ts
+++ common/components/elements/BannerSection/styled/Container.styled.ts
@@ -1,7 +1,18 @@
 import styled from "styled-components";
 
+import { MEDIA } from "@constants";
+
 import ContentContainer from "@components/elements/ContentContainer";
 
-const Container = styled(ContentContainer)``;
+const Container = styled(ContentContainer)`
+  ${MEDIA.MD} {
+    display: flex;
+    flex-direction: row;
+    gap: 2rem;
+  }
+
+  ${MEDIA.LG} {
+  }
+`;
 
 export default Container;
