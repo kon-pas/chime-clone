32f19f0bc81cef2aec67f9763596557d37766626
kon-pas <konpas@mail.com>
2022-10-19 17:18:10 +0200

    feat(constants): dynamically produce `BREAKPOINTS`

--- common/constants/breakpoints.ts
+++ common/constants/breakpoints.ts
@@ -1,8 +1,19 @@
-const BREAKPOINTS: {
-  [key: string]: number;
-} = {
-  MD: 860,
-  LG: 1100,
-};
+import { breakpoints } from "@enums";
+
+/**
+ * Creates an object from enum with all keys being upper case.
+ */
+const BREAKPOINTS: {readonly [key: string]: number} = Object.entries(breakpoints).reduce(
+  (prev, [key, value]) =>
+    typeof value === "number" && { [key.toUpperCase()]: value, ...prev },
+  {}
+);
+
+// const BREAKPOINTS: {
+//   [key: string]: number;
+// } = {
+//   MD: 860,
+//   LG: 1100,
+// };
 
 export default BREAKPOINTS;
