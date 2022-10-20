ef6355d479114a6e015b93e9111dd3eefd08e4b3
kon-pas <konpas@mail.com>
2022-10-20 11:46:31 +0200

    refactor(utils): add tsdoc

---
+++ common/utils/get-resize-transition.ts
@@ -0,0 +1,19 @@
+/**
+ * Shorthand for css property intended for all screen resize transitions
+ * 
+ * @param properties - properties to which apply transition
+ * 
+ * @returns css transition property
+ * 
+ * @example
+ * Sample usage in styled-components:
+ * ```
+ * ${getResizeTransition('margin', 'padding)}
+ * ```
+ */
+const getResizeTransition = (...properties: string[]): string =>
+  `transition: ${properties.map((property, idx, properties) =>
+    `${property} 0.35s ease-out${idx === properties.length - 1 ? ";" : ","}`
+  )}`;
+
+export default getResizeTransition;
--- common/utils/use-resize-transition.ts
+++
@@ -1,6 +0,0 @@
-const resizeTransition = (...properties: string[]): string =>
-  `transition: ${properties.map((property, idx, properties) =>
-    `${property} 0.35s ease-out${idx === properties.length - 1 ? ";" : ","}`
-  )}`;
-
-export default resizeTransition;
