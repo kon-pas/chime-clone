a1a5e6219a8681aee6465b2037e86ffec49b74cf
kon-pas <konpas@mail.com>
2022-10-21 04:32:21 +0200

    init(pages): `/debit-card`

---
+++ pages/debit-card.tsx
@@ -0,0 +1,26 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+import MonoSection from "@components/sections/MonoSection";
+import DualSection from "@components/sections/DualSection";
+import BannerSection from "@components/sections/BannerSection";
+
+const DebitCard: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Free Visa Debit Card | Chime</title>
+        <meta name="description" content="Chime Clone Debit Card" />
+      </Head>
+
+      {/* <BannerSection
+      img={{
+        src
+      }}>
+
+      </BannerSection> */}
+    </>
+  );
+};
+
+export default DebitCard;
