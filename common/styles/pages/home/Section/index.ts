7c900349355461405ac2a42f6733df3f127789ad
kon-pas <konpas@mail.com>
2022-10-22 08:58:53 +0200

    refactor(`/home`): move styles to `/components`

--- common/styles/pages/home/Section/index.ts
+++
@@ -1,9 +0,0 @@
-export { default as Wrapper } from "./Wrapper.styled";
-
-export { default as Container } from "./Container.styled";
-
-export { default as ImageWrapper } from "./ImageWrapper.styled";
-
-export { default as TextContainer } from "./TextContainer.styled";
-
-export { default as Title } from "./Title.styled";
