002d34fb1f5eb8433359a14de8568b0fa6f0ab94
kon-pas <konpas@mail.com>
2022-10-23 22:52:24 +0200

    add(`/faq`): `Section` component

---
+++ common/components/pages/faq/Section.styled.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+import { COLORS } from "@constants";
+
+const Section = styled.div`
+  background-color: ${COLORS.GRAY_LIGHT};
+  padding: 5rem 0;
+`;
+
+export default Section;
