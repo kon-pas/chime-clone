a82d4c488fb5b321c0817bdbf103c22e77c292c1
kon-pas <konpas@mail.com>
2022-10-23 20:03:10 +0200

    add(`ToggleBox`): animation

--- common/components/elements/ToggleBox/styled/Modal.styled.ts
+++ common/components/elements/ToggleBox/styled/Modal.styled.ts
@@ -1,11 +1,26 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+
+const show = keyframes`
+  from {
+    visibility: hidden;
+    opacity: 0;
+    max-height: 0vh;
+  }
+
+  to {
+    visibility: none;
+    opacity: 1;
+    max-height: 100vh;
+  }
+`;
 
 interface ModalProps {
-  isVisible: boolean;
+  isOpened: boolean;
 }
 
 const Modal = styled.div<ModalProps>`
-  display: ${props => props.isVisible ? "block" : "none"};
+  animation: ${show} 0.5s ease-out;
+  padding-bottom: 1.5rem;
 `;
 
 export default Modal;
