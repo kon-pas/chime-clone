da4a2707ee5d88b5bb73907e344983cfec3e9296
kon-pas <konpas@mail.com>
2022-10-24 01:46:39 +0200

    init(components): `Headings`

---
+++ common/components/elements/Headings/Headings.styled.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+import { h1, h2, h3 } from "@styles/Tags.styled";
+
+export const H1 = styled.h1`
+  ${h1}
+`;
+
+export const H2 = styled.h2`
+  ${h2}
+`;
+
+export const H3 = styled.h3`
+  ${h3}
+`;
