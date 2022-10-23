02968283573a800e9f6596a996f3443950a363f5
kon-pas <konpas@mail.com>
2022-10-24 01:45:22 +0200

    add: new paths

--- common/constants/navigation-items.ts
+++ common/constants/navigation-items.ts
@@ -3,104 +3,106 @@ import type { NavigationItem } from "@interfaces";
 const NAVIGATION_ITEMS: NavigationItem[] = [
   {
     label: "Benefits",
+    path: "/no-fees",
     subitems: [
       {
-        label: 'No Monthly Fees',
-        path: '/no-fees'
+        label: "No Monthly Fees",
+        path: "/no-fees",
       },
       {
-        label: 'Free Debit Card',
-        path: '/debit-card'
+        label: "Free Debit Card",
+        path: "/debit-card",
       },
       {
-        label: 'Fee-Free Overdraft',
+        label: "Fee-Free Overdraft",
       },
       {
-        label: 'Build Credit',
+        label: "Build Credit",
       },
       {
-        label: 'Get Paid Early',
-        path: '/get-paid-early'
+        label: "Get Paid Early",
+        path: "/get-paid-early",
       },
       {
-        label: '60,000+ Fee-Free ATMs',
+        label: "60,000+ Fee-Free ATMs",
       },
       {
-        label: 'High Yield Savings Account',
-        path: '/savings-account',
+        label: "High Yield Savings Account",
+        path: "/savings-account",
       },
       {
-        label: 'Send and Receive Money',
+        label: "Send and Receive Money",
       },
       {
-        label: 'Security and Control',
-        path: '/security-and-control'
-      }
+        label: "Security and Control",
+        path: "/security-and-control",
+      },
     ],
   },
   {
     label: "Accounts",
+    path: "/savings-account",
     subitems: [
       {
-        label: 'Checking Account',
+        label: "Checking Account",
       },
       {
-        label: 'Credit Builder',
+        label: "Credit Builder",
       },
       {
-        label: 'Savings Account',
-        path: '/savings-account'
-      }
+        label: "Savings Account",
+        path: "/savings-account",
+      },
     ],
   },
   {
-    label: 'Who we are',
-    path: '/about-us',
+    label: "Who we are",
+    // path: '/about-us',
     subitems: [
       {
-        label: 'About Us',
-        path: '/about-us',
+        label: "About Us",
+        // path: '/about-us',
       },
       {
-        label: 'In the News',
+        label: "In the News",
       },
       {
-        label: 'Chime Financial, Inc.',
+        label: "Chime Financial, Inc.",
       },
       {
-        label: 'In The Community',
+        label: "In The Community",
       },
       {
-        label: 'Careers',
-      }
-    ]
+        label: "Careers",
+      },
+    ],
   },
   {
     label: "Learn",
     path: '/faq',
     subitems: [
       {
-        label: 'Blog',
+        label: "Blog",
       },
       {
-        label: 'Help Center',
+        label: "Help Center",
       },
       {
-        label: 'FAQs',
-        path: '/faq'
+        label: "FAQs",
+        path: "/faq",
       },
       {
-        label: 'Online Banking',
+        label: "Online Banking",
       },
       {
-        label: 'Second Chance Banking',
-        path: '/second-chance-banking'
+        label: "Second Chance Banking",
+        path: "/second-chance-banking",
       },
       {
-        label: 'Tax Refund',
-      }
-    ]
-  }
+        label: "Tax Refund",
+      },
+    ],
+  },
 ];
 
 export default NAVIGATION_ITEMS;
