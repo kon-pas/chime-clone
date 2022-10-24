9502d0d744e719df17c75fcd89dddcc38ce2604d
kon-pas <konpas@mail.com>
2022-10-24 21:53:18 +0200

    style: `ModalLayout`

--- common/components/composition/ModalLayout/styled/Container.styled.ts
+++ common/components/composition/ModalLayout/styled/Container.styled.ts
@@ -1,8 +1,12 @@
 import styled from "styled-components";
 
 const Container = styled.div`
-  width: 50vw;
+  width: 40vw;
   margin: 2rem auto 0 auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
 `;
 
 export default Container;
