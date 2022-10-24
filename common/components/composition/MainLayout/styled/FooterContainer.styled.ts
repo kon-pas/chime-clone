ca807c95fea47ade12f61cc6937b55d55082c54a
kon-pas <konpas@mail.com>
2022-10-24 19:57:37 +0200

    chore(components): rename `Layout` to `MainLayout`

---
+++ common/components/composition/MainLayout/styled/FooterContainer.styled.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+const FooterContainer = styled.div`
+  margin-top: auto;
+  min-height: 50px;
+`;
+
+export default FooterContainer;
---
+++ common/components/composition/MainLayout/styled/PageContainer.styled.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const PageContainer = styled.div`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
+
+export default PageContainer;
