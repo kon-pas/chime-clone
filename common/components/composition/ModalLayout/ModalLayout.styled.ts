422e24a86ec48f1998c37841e73c33100cd02c83
kon-pas <konpas@mail.com>
2022-10-25 23:57:11 +0200

    add(`ModalLayout`): styles

---
+++ common/components/composition/ModalLayout/ModalLayout.styled.ts
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 35vw;
+  margin: 2rem auto 0 auto;
+`;
--- common/components/composition/ModalLayout/styled/Container.styled.ts
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-const Container = styled.div`
-  width: 40vw;
-  margin: 2rem auto 0 auto;
-`;
-
-export default Container;
