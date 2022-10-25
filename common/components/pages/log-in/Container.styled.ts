2e438684b830e4fc625f95c51a199c8058d594b0
kon-pas <konpas@mail.com>
2022-10-25 20:38:52 +0200

    add(`/log-in`): `Container` component

---
+++ common/components/pages/log-in/Container.styled.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+`;
+
+export default Container;
--- common/components/pages/log-in/index.ts
+++ common/components/pages/log-in/index.ts
@@ -3,3 +3,5 @@ export { default as Text1 } from "./Text1.styled";
 export { default as Text2 } from "./Text2.styled";
 
 export { default as Text3 } from "./Text3.styled";
+
+export { default as Container } from "./Container.styled";
