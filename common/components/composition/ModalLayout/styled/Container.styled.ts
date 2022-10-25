be509bb3ec36c08163f80e8ed15ec6c3f0700d42
kon-pas <konpas@mail.com>
2022-10-25 20:39:16 +0200

    style(`ModalLayout`): `Container`

--- common/components/composition/ModalLayout/styled/Container.styled.ts
+++ common/components/composition/ModalLayout/styled/Container.styled.ts
@@ -3,10 +3,6 @@ import styled from "styled-components";
 const Container = styled.div`
   width: 40vw;
   margin: 2rem auto 0 auto;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  text-align: center;
 `;
 
 export default Container;
