8a78a045fe3766ed1a7af5bad19ccf5367e94580
kon-pas <konpas@mail.com>
2022-10-26 20:49:31 +0200

    add(constants): new colors and reorganize

--- common/constants/colors.ts
+++ common/constants/colors.ts
@@ -1,17 +1,24 @@
-import type { HexColor } from '@types';
+import type { HexColor } from "@types";
 
-const COLORS: {[key: string]: HexColor} = {
-  FONT_GRAY_LIGHT: '#74758C',
-  FONT_GRAY_MEDIUM: '#4A4A4A',
-  FONT_GRAY_DARK: '#333333',
-  GRAY_LIGHT: '#FAFAFA',
-  GRAY_MEDIUM: '#D5D5D5',
-  LIME_LIGHT: '#F0FBF5',
-  LIME_MEDIUM: '#2DE28D',
-  LIME_DARK: '#1EC677',
-  WHITE: '#FFFFFF',
-  BLACK: '#000000',
-  CYAN_LIGTH: '#EBFFFB',
-}
+const COLORS: { [key: string]: HexColor } = {
+  FONT_GRAY_LIGHT: "#74758C",
+  FONT_GRAY_MEDIUM: "#4A4A4A",
+  FONT_GRAY_DARK: "#333333",
+  FONT_WARN: "#f2a9a6",
+
+  BG_WARN: "#fdf4f0",
+
+  GRAY_LIGHT: "#FAFAFA",
+  GRAY_MEDIUM: "#D5D5D5",
+
+  LIME_LIGHT: "#F0FBF5",
+  LIME_MEDIUM: "#2DE28D",
+  LIME_DARK: "#1EC677",
+
+  CYAN_LIGTH: "#EBFFFB",
+
+  WHITE: "#FFFFFF",
+  BLACK: "#000000",
+};
 
 export default COLORS;
