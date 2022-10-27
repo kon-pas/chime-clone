535803829c776ac686d6b8b38799423a1d32d960
kon-pas <konpas@mail.com>
2022-10-27 22:06:28 +0200

    init(database): `users`

---
+++ common/database/users.ts
@@ -0,0 +1,12 @@
+/**
+ * Available via `/api/database/users` as JSON
+ */
+const users = [
+  {
+    username: "Jan_Kowalski",
+    email: "jan@kowalski.com",
+    password: "kowal123",
+  },
+];
+
+export default users;
