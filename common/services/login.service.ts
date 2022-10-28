06a11133ece46a210694c2a41362ec1cf76823b7
kon-pas <konpas@mail.com>
2022-10-28 23:05:02 +0200

    add: types, error handling & data fetching

--- common/services/login.service.ts
+++ common/services/login.service.ts
@@ -2,21 +2,27 @@
 
 import { Email, Password } from "@types";
 
+const { LOCALHOST } = process.env;
+
 interface LoginValues {
   email: Email;
   password: Password;
 }
 
-type loginSignature = (user: LoginValues) => Promise<boolean>
+type loginSignature = (user: LoginValues) => Promise<boolean>;
 
-const login: loginSignature = async (user) => {
-  const res = await fetch("/api/users/log-in", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ ...user }),
-  }).then(res => res.json());
+const login: loginSignature = async user => {
+  try {
+    const users = await fetch(`/api/users/log-in`
+      // method: "GET",
+      // headers: { "Content-Type": "application/json; charset=utf8" },
+      // body: JSON.stringify({ ...user }),
+    ).then(res => res.json());
+  } catch (error) {
+    console.error(error);
+  }
 
-  return res.error;
+  return true;
 };
 
 export default login;
