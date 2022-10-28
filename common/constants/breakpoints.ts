b266fc5a8464d3445aab42e3584e73e7f2e71284
kon-pas <konpas@mail.com>
2022-10-28 23:50:50 +0200

    refactor(`breakpoints`): remove old code

--- common/constants/breakpoints.ts
+++ common/constants/breakpoints.ts
@@ -1,19 +1,16 @@
 import { breakpoints } from "@enums";
 
+interface Breakpoints {
+  readonly [key: string]: number;
+}
+
 /**
  * Creates an object from enum with all keys being upper case.
  */
-const BREAKPOINTS: {readonly [key: string]: number} = Object.entries(breakpoints).reduce(
+const BREAKPOINTS: Breakpoints = Object.entries(breakpoints).reduce(
   (prev, [key, value]) =>
     typeof value === "number" && { [key.toUpperCase()]: value, ...prev },
   {}
 );
-
-// const BREAKPOINTS: {
-//   [key: string]: number;
-// } = {
-//   MD: 860,
-//   LG: 1100,
-// };
 
 export default BREAKPOINTS;
