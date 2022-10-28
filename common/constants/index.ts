58381e9d5422721f06803416499de2cc101f8fa6
kon-pas <konpas@mail.com>
2022-10-28 23:54:10 +0200

    refactor(constants):`prettier` & deprecate `media`

--- common/constants/index.ts
+++ common/constants/index.ts
@@ -1,11 +1,14 @@
-export { default as COLORS }  from './colors';
+export { default as COLORS } from "./colors";
 
-export { default as NAVIGATION_ITEMS } from './navigation-items';
+export { default as NAVIGATION_ITEMS } from "./navigation-items";
 
-export { default as BREAKPOINTS } from './breakpoints';
+export { default as BREAKPOINTS } from "./breakpoints";
 
+export { default as Z_INDEX } from "./z-index";
+
+export { default as HTTP } from "./http";
+
+/**
+ * Deprecated
+ */
 // export { default as MEDIA } from './media';
-
-export { default as Z_INDEX } from './z-index';
-
-export { default as HTTP } from './http';
