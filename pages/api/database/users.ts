34099b340b5e7b36cef7b94376217040f44aa3b5
kon-pas <konpas@mail.com>
2022-10-29 22:23:04 +0200

    add(api): error handling

--- pages/api/database/users.ts
+++ pages/api/database/users.ts
@@ -1,19 +1,24 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
+import type { HttpStatus, User } from "@interfaces";
+
+import { HTTP } from "@constants";
+
 import { users } from "@database";
 
-type handlerSignature = (req: NextApiRequest, res: NextApiResponse) => void;
+type Data = HttpStatus | User[];
+
+type handlerSignature = (
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) => void;
 
 const handler: handlerSignature = async (req, res) => {
   try {
-    if(req.method == "GET")
-      res.status(200).send({ ...users });
+    if (req.method === "GET") res.status(200).send([...users]);
   } catch (error) {
     console.error(error);
-    console.log(2);
-    res
-      .status(500)
-      .send({ statusCode: 500, statusMessage: "Internal Server Error" });
+    res.status(500).send(HTTP.STATUS_500);
   }
 };
 
