8d1364a389b72c388b031ea2046bb99f30fd49e2
kon-pas <konpas@mail.com>
2022-10-29 22:19:46 +0200

    add(database): sample users

--- common/database/users.ts
+++ common/database/users.ts
@@ -1,4 +1,4 @@
-import type { User } from "@interfaces"
+import type { User } from "@interfaces";
 
 /**
  * Available via `/api/database/users` as JSON
@@ -8,6 +8,16 @@ const users: User[] = [
     username: "Jan_Kowalski",
     email: "jan@kowalski.com",
     password: "kowal123",
+  },
+  {
+    username: "Jan_Nowak",
+    email: "jan@nowak.com",
+    password: "nowak123",
+  },
+  {
+    username: "Jan_Jankowski",
+    email: "jan@jankowski.com",
+    password: "janek123",
   },
 ];
 
