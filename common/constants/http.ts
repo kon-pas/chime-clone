4697215029a7fc6a3cac2c400856a0ebcae256c0
kon-pas <konpas@mail.com>
2022-10-29 22:19:28 +0200

    add(constants): `http` types

--- common/constants/http.ts
+++ common/constants/http.ts
@@ -1,8 +1,14 @@
-const HTTP = {
+import type { HttpStatus } from "@interfaces";
+
+const HTTP: { [key: string]: HttpStatus } = {
+  STATUS_404: {
+    statusCode: 404,
+    statusMessage: "Not Found",
+  },
   STATUS_500: {
     statusCode: 500,
-    statusMessage: "Internal Server Error"
-  }
-}
+    statusMessage: "Internal Server Error",
+  },
+};
 
 export default HTTP;
