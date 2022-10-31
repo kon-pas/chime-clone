ab4b7a3bea8215a703f0d82eb17c21599d6fb1cf
kon-pas <konpas@mail.com>
2022-10-31 23:47:37 +0100

    init(utils): `ErrorResponse`

--- common/utils/api/ErrorResponse.ts
+++ common/utils/api/ErrorResponse.ts
@@ -1,9 +1,31 @@
 import type { HttpResponseStatus } from "@interfaces";
 
-export default class ErrorResponse {
-  public readonly status: HttpResponseStatus;
+import type { HttpResponseStatusCode } from "@types";
 
-  constructor(status: HttpResponseStatus) {
-    this.status = status;
+import { HTTP } from "@constants";
+
+export default class ErrorResponse {
+  public readonly statusCode: HttpResponseStatusCode;
+
+  constructor(statusCode: HttpResponseStatusCode) {
+    this.statusCode = statusCode;
+  }
+
+  get statusMessage(): string {
+    for (const [_, { code, message }] of Object.entries(HTTP)) {
+      if (this.statusCode === code) return message;
+    }
+
+    // @@@ TODO: Get rid of this hard-coded return, although it is
+    // never reached as long as `HTTP` contains objects to all
+    // corresponding `HttpResponseStatusCode`s
+    return "Wrong code";
+  }
+
+  get status(): HttpResponseStatus {
+    return {
+      code: this.statusCode,
+      message: this.statusMessage,
+    };
   }
 }
