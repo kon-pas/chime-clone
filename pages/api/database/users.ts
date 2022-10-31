1a1ad60b344532e8140a306d52761d0e4905be05
kon-pas <konpas@mail.com>
2022-10-31 21:43:02 +0100

    chore(api): cleanup

--- pages/api/database/users.ts
+++ pages/api/database/users.ts
@@ -1,23 +1,20 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
-import type { HttpStatus, FullUser } from "@interfaces";
+import type { HttpResponseStatus, FullUser } from "@interfaces";
 
 import { HTTP } from "@constants";
 
 import { users } from "@database";
 
-type Data = HttpStatus | FullUser[];
-
 type handlerSignature = (
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<HttpResponseStatus | FullUser[]>
 ) => void;
 
 const handler: handlerSignature = async (req, res) => {
   try {
     if (req.method === "GET") res.status(200).send([...users]);
   } catch (error) {
-    console.error(error);
     res.status(500).send(HTTP.STATUS_500);
   }
 };
