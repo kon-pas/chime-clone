1971b25eb1379a8767eddd28ee17988091acb02f
kon-pas <konpas@mail.com>
2022-10-31 23:45:15 +0100

    add(constants): http status 200

--- common/constants/http.ts
+++ common/constants/http.ts
@@ -1,6 +1,10 @@
 import type { HttpResponseStatus } from "@interfaces";
 
 const HTTP: { [key: string]: HttpResponseStatus } = {
+  STATUS_200: {
+    code: 200,
+    message: "OK",
+  },
   STATUS_404: {
     code: 404,
     message: "Not Found",
