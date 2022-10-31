43b44d08a5ddf8ee8cc8ecd9fa1dd4307a6c1508
kon-pas <konpas@mail.com>
2022-10-31 21:43:54 +0100

    chore(utils): exports cleanup

---
+++ common/utils/api/index.ts
@@ -0,0 +1,1 @@
+export * as fetchWrapper from "./fetch-wrapper";
--- common/utils/index.ts
+++ common/utils/index.ts
@@ -3,5 +3,3 @@ export { default as getResizeTransition } from "./get-resize-transition";
 export { default as keysToLowercase } from "./keys-to-lowercase";
 
 export { default as getMedia } from "./get-media";
-
-export * as fetchWrapper from "./fetch-wrapper";
