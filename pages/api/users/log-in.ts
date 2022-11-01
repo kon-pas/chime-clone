ea1e62e8e33a1321929d7746857c030c36ea5f03
kon-pas <konpas@mail.com>
2022-11-01 23:17:31 +0100

    fix(api): handle responses with `@utils/api`

--- pages/api/users/log-in.ts
+++ pages/api/users/log-in.ts
@@ -1,12 +1,10 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
-import { fetchWrapper } from "@utils/api";
-
-import { HTTP } from "@constants";
-
 import type { Response } from "@types";
 
-import { ErrorResponse, LoadedResponse } from "@utils/api";
+import type { FullUser } from "@interfaces";
+
+import { ErrorResponse, LoadedResponse, fetchWrapper } from "@utils/api";
 
 type handlerSignature = (
   req: NextApiRequest,
@@ -17,28 +15,34 @@ const handler: handlerSignature = async (req, res) => {
   const { NEXT_PUBLIC_API_URL } = process.env;
 
   try {
-    const response = fetchWrapper.get({
-      url: `${NEXT_PUBLIC_API_URL}/database/users`,
-    });
-
-    const users = await response.then(res => res.json());
+    const response: Response = await fetchWrapper
+      .get({
+        url: `${NEXT_PUBLIC_API_URL}/database/users`,
+      })
+      .then(res => res.json());
 
     if (req.method === "POST") {
       const { email: targetEmail, password: targetPassword } = req.body;
 
-      if (users?.statusCode === 500)
+      if (response instanceof LoadedResponse) {
+        const { body } = response as any;
+
+        const targetUser: FullUser | undefined = body.find(
+          (user: FullUser) =>
+            user.email === targetEmail && user.password === targetPassword
+        );
+
+        if (targetUser)
+          res.status(200).send(
+            new LoadedResponse(200, {
+              username: targetUser.username,
+              email: targetUser.email,
+            })
+          );
+        else res.status(404).send(new ErrorResponse(404));
+      } else if (response instanceof ErrorResponse) {
         res.status(500).send(new ErrorResponse(500));
-
-      const targetUser = users.find(
-        (user: FullUser) =>
-          user.email === targetEmail && user.password === targetPassword
-      );
-
-      if (targetUser)
-        res
-          .status(200)
-          .send({ username: targetUser.username, email: targetUser.email });
-      else res.status(404).send(new ErrorResponse(404));
+      }
     }
   } catch (error) {
     res.status(500).send(new ErrorResponse(500));
