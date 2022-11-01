67814598981bf87b84463bcdeeb2cdec2d6ca03a
kon-pas <konpas@mail.com>
2022-11-01 23:21:08 +0100

    refactor(utils): remove unused code

--- common/utils/api/LoadedResponse.ts
+++ common/utils/api/LoadedResponse.ts
@@ -3,14 +3,10 @@ import { ErrorResponse } from "@utils/api";
 import { HttpResponseStatusCode } from "@types";
 
 export default class LoadedResponse extends ErrorResponse {
-  private _body: string;
+  public readonly body: object;
 
   constructor(statusCode: HttpResponseStatusCode, body: object) {
     super(statusCode);
-    this._body = JSON.stringify(body);
-  }
-
-  get body(): object {
-    return JSON.parse(this._body);
+    this.body = body;
   }
 }
