30a643461ca549eda4886e55bdce661399aa4833
kon-pas <konpas@mail.com>
2022-11-03 23:38:01 +0100

    chore(api): export `HttpResponse`

--- common/utils/api/index.ts
+++ common/utils/api/index.ts
@@ -3,3 +3,5 @@ export * as fetchWrapper from "./fetch-wrapper";
 export { default as ErrorResponse } from "./ErrorResponse";
 
 export { default as LoadedResponse } from "./LoadedResponse";
+
+export { default as HttpResponse } from "./HttpResponse";
