d2d0fbc2c5d099c36449eb8a4d2a4eb5feec4261
kon-pas <konpas@mail.com>
2022-11-04 23:33:16 +0100

    init(services): `register`

--- common/services/register.service.ts
+++ common/services/register.service.ts
@@ -1,1 +1,41 @@
-// move to fetch-wrapper
+import type { Username, Email, Password } from "@types";
+
+import { fetchWrapper, HttpResponse } from "@utils/api";
+
+interface RegisterValues {
+  username: Username;
+  email: Email;
+  password: Password;
+}
+
+type loginSignature = (
+  loginData: RegisterValues,
+  onSuccess?: () => void,
+  onFailure?: () => void
+) => Promise<boolean>;
+
+const login: loginSignature = async (registerData, onSuccess, onFailure) => {
+  const { NEXT_PUBLIC_API_URL } = process.env;
+
+  try {
+    const response: HttpResponse = await fetchWrapper
+      .post({
+        url: `${NEXT_PUBLIC_API_URL}/users/sign-up`,
+        body: JSON.stringify(registerData),
+      })
+      .then(res => res.json());
+
+    if (response.success) {
+      onSuccess && onSuccess();
+      return true;
+    } else {
+      onFailure && onFailure();
+      return false;
+    }
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
+};
+
+export default login;
