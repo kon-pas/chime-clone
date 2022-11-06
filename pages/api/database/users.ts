b3089b837f3432b000a519c248808084d1413d25
kon-pas <konpas@mail.com>
2022-11-06 22:57:27 +0100

    add(`api/database`): `put` method type

--- pages/api/database/users.ts
+++ pages/api/database/users.ts
@@ -1,5 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
+import type { FullUser } from "@interfaces";
+
 import { HttpResponse } from "@utils/api";
 
 import { USERS } from "@database";
@@ -10,15 +12,24 @@ type handlerSignature = (
 ) => void;
 
 /**
- * Imitating `users` document in database.
+ * Imitating database.
  */
 const handler: handlerSignature = async (req, res) => {
   switch (req.method) {
     case "GET":
       res.status(200).send(new HttpResponse(200, [...USERS]));
-    case "PUT":
-      USERS.push(req.body);
-      res.status(201).send(new HttpResponse(201));
+    case "PUT": {
+      const { username, email, password } = req.body as FullUser;
+
+      if (
+        USERS.some(user => user.username === username || user.email === email)
+      ) {
+        res.status(500).send(new HttpResponse(500));
+      } else {
+        USERS.push({ username, email, password });
+        res.status(201).send(new HttpResponse(201));
+      }
+    }
     default:
       res.status(500).send(new HttpResponse(500));
   }
