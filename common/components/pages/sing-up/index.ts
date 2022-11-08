1954cd2de5797122c48bd55b04e1677e6bb68933
kon-pas <konpas@mail.com>
2022-11-08 23:53:47 +0100

    chore(`/sign-up`): export styles

---
+++ common/components/pages/sing-up/index.ts
@@ -0,0 +1,11 @@
+export * as Form from "./Heading.styled";
+
+export * as Disclaimer from "./Heading.styled";
+
+export { default as Heading } from "./Heading.styled";
+
+export { default as Text1 } from "./Heading.styled";
+
+export { default as Text2 } from "./Heading.styled";
+
+export { default as Anchor } from "./Heading.styled";
