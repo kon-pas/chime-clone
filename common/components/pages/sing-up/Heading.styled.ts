c45d5d28ab37f7c6c8aba0fbbd3729cbc59a109b
kon-pas <konpas@mail.com>
2022-11-09 23:21:30 +0100

    wip(pages): styles

---
+++ common/components/pages/sing-up/Heading.styled.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+const Heading = styled.div`
+
+`;
+
+export default Heading;
--- common/components/pages/sing-up/index.ts
+++ common/components/pages/sing-up/index.ts
@@ -2,6 +2,8 @@ export * as Form from "./Heading.styled";
 
 export * as Disclaimer from "./Heading.styled";
 
+export { default as Container } from "./Container.styled";
+
 export { default as Heading } from "./Heading.styled";
 
 export { default as Text1 } from "./Heading.styled";
