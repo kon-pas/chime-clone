73fe5f13899a30bcf4bcb0f63e7a822d7ed5d283
kon-pas <konpas@mail.com>
2022-11-10 10:56:31 +0100

    style(`/sign-up`): `Disclaimer`

--- common/components/pages/sing-up/Disclaimer/Container.styled.ts
+++ common/components/pages/sing-up/Disclaimer/Container.styled.ts
@@ -1,5 +1,10 @@
 import styled from "styled-components";
 
-const Container = styled.div``;
+const Container = styled.div`
+  margin-top: 7.5rem;
+  display: flex;
+  flex-direction: column;
+  gap: 3rem;
+`;
 
 export default Container;
--- common/components/pages/sing-up/Disclaimer/Item.styled.ts
+++ common/components/pages/sing-up/Disclaimer/Item.styled.ts
@@ -1,5 +1,11 @@
 import styled from "styled-components";
 
-const Item = styled.div``;
+import { COLORS } from "@constants";
+
+const Item = styled.div`
+  font-size: 0.65rem;
+  line-height: 1.6em;
+  color: ${COLORS.FONT_GRAY_LIGHT};
+`;
 
 export default Item;
