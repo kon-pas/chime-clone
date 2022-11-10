f28774349e262c84497ea4aaae7172a381a9026c
kon-pas <konpas@mail.com>
2022-11-10 12:47:15 +0100

    style(pages): `/sign-up`

--- common/components/pages/sing-up/Form/Splitter.styled.ts
+++ common/components/pages/sing-up/Form/Splitter.styled.ts
@@ -3,6 +3,7 @@ import styled from "styled-components";
 const Splitter = styled.div`
   display: grid;
   grid-auto-flow: column;
+  grid-auto-columns: 1fr;
   gap: 0.3rem;
 `;
 
--- common/components/pages/sing-up/Text2.styled.ts
+++ common/components/pages/sing-up/Text2.styled.ts
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import { COLORS } from "@constants";
 
 const Text2 = styled.div`
-  margin: 0.5rem 0 2rem 0;
+  margin: 0.75rem 0 2rem 0;
   font-size: 0.8rem;
   color: ${COLORS.FONT_GRAY_LIGHT};
 `;
