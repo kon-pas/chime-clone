b3fb1e5ad1081c2edf79f30412f79c8d3daa8724
kon-pas <konpas@mail.com>
2022-11-10 10:54:44 +0100

    add(pages): content to `/sign-up`

--- pages/users/sign-up.tsx
+++ pages/users/sign-up.tsx
@@ -61,7 +61,79 @@ const SignUpPage: NextPageWithLayout = () => {
         Let&apos;s start with some basic information.
       </SignUpComponents.Text1>
 
-      {/* Form */}
+      <SignUpComponents.Form.Container onSubmit={handleSubmit(() => {})}>
+        {/* @@@ TODO: handle submit */}
+        <SignUpComponents.Form.Splitter>
+          <SignUpComponents.Form.Input
+            type="email"
+            placeholder="First name"
+            {...register("email", {
+              required: "required",
+              pattern: {
+                value: /\S+@\S+\.\S+/,
+                message: "Entered value does not match email format",
+              },
+            })}
+          />
+          {errors.email && (
+            <SignUpComponents.Form.Error>
+              Please correct your email address
+            </SignUpComponents.Form.Error>
+          )}
+
+          <SignUpComponents.Form.Input
+            type="email"
+            placeholder="First name"
+            {...register("email", {
+              required: "required",
+              pattern: {
+                value: /\S+@\S+\.\S+/,
+                message: "Entered value does not match email format",
+              },
+            })}
+          />
+          {errors.email && (
+            <SignUpComponents.Form.Error>
+              Please correct your email address
+            </SignUpComponents.Form.Error>
+          )}
+        </SignUpComponents.Form.Splitter>
+        <SignUpComponents.Form.Input
+          type="email"
+          placeholder="First name"
+          {...register("email", {
+            required: "required",
+            pattern: {
+              value: /\S+@\S+\.\S+/,
+              message: "Entered value does not match email format",
+            },
+          })}
+        />
+        {errors.email && (
+          <SignUpComponents.Form.Error>
+            Please correct your email address
+          </SignUpComponents.Form.Error>
+        )}
+        <SignUpComponents.Form.Input
+          type="password"
+          placeholder="Password"
+          {...register("password", {
+            required: "required",
+            minLength: {
+              value: 5,
+              message: "min length is 5",
+            },
+          })}
+        />
+        {errors.password && (
+          <SignUpComponents.Form.Error>
+            Please correct your password
+          </SignUpComponents.Form.Error>
+        )}
+        <SignUpComponents.Form.Submit disabled={isSubmitting} type="submit">
+          Sign Up
+        </SignUpComponents.Form.Submit>
+      </SignUpComponents.Form.Container>
 
       <SignUpComponents.Text2>
         You can change your preffered name later in your profile.
@@ -70,52 +142,44 @@ const SignUpPage: NextPageWithLayout = () => {
       <SignUpComponents.Anchor>
         Already have an account?
       </SignUpComponents.Anchor>
+
+      <SignUpComponents.Disclaimer.Container>
+        <SignUpComponents.Disclaimer.Item>
+          By applying for a Chime Checking Account, you are providing your
+          contact information to The Bancorp Bank or Stride Bank N.A. and Chime
+          for their own separate uses, including marketing. Please review The
+          Bancorp Bank&apos;s Privacy Policy, Stride Bank N.A. Privacy Policy,
+          and Chime&apos;s Privacy Policy.
+        </SignUpComponents.Disclaimer.Item>
+
+        <SignUpComponents.Disclaimer.Item>
+          IMPORTANT INFORMATION ABOUT PROCEDURES FOR OPENING A NEW ACCOUNT: To
+          help the government fight the funding of terrorism and money
+          laundering activities, federal law requires all financial institutions
+          to obtain, verify, and record information that identifies each person
+          who opens an Account. What this means for you: When you open an
+          Account, we will ask for your name, address, date of birth, and other
+          information that will allow us to identify you. We may also ask to see
+          a copy of your driver&apos;s license or other identifying documents.
+        </SignUpComponents.Disclaimer.Item>
+
+        <SignUpComponents.Disclaimer.Item>
+          © 2022 Chime. All Rights Reserved.
+        </SignUpComponents.Disclaimer.Item>
+
+        <SignUpComponents.Disclaimer.Item>
+          Banking Services provided by The Bancorp Bank or Stride Bank, N.A.,
+          Members FDIC. The Chime Visa® Debit Card is issued by The Bancorp Bank
+          or Stride Bank, N.A. pursuant to a license from Visa U.S.A. Inc. and
+          may be used everywhere Visa debit cards are accepted.
+        </SignUpComponents.Disclaimer.Item>
+      </SignUpComponents.Disclaimer.Container>
     </SignUpComponents.Container>
-
-    //   <LogInComponents.Form.Container>  {/* handle submit */}
-    //     <LogInComponents.Form.Input
-    //       type="email"
-    //       placeholder="Email address"
-    //       {...register("email", {
-    //         required: "required",
-    //         pattern: {
-    //           value: /\S+@\S+\.\S+/,
-    //           message: "Entered value does not match email format",
-    //         },
-    //       })}
-    //     />
-    //     {errors.email && (
-    //       <LogInComponents.Form.Error>
-    //         Please correct your email address
-    //       </LogInComponents.Form.Error>
-    //     )}
-
-    //     <LogInComponents.Form.Input
-    //       type="password"
-    //       placeholder="Password"
-    //       {...register("password", {
-    //         required: "required",
-    //         minLength: {
-    //           value: 5,
-    //           message: "min length is 5",
-    //         },
-    //       })}
-    //     />
-    //     {errors.email && (
-    //       <LogInComponents.Form.Error>
-    //         Please correct your password
-    //       </LogInComponents.Form.Error>
-    //     )}
-
-    //     <LogInComponents.Form.Submit disabled={isSubmitting} type="submit">
-    //       Login
-    //     </LogInComponents.Form.Submit>
-    //   </LogInComponents.Form.Container>
   );
 };
 
 SignUpPage.getLayout = (page: ReactElement) => (
-  <ModalLayout>{page}</ModalLayout>
+  <ModalLayout width={90}>{page}</ModalLayout>
 );
 
 export default SignUpPage;
