42a40e9b87db6d4384d02eb60321d829d2d6f0a5
kon-pas <konpas@mail.com>
2022-11-14 03:10:28 +0100

    feat(database): handle request authorization

--- pages/api/database/users.ts
+++ pages/api/database/users.ts
@@ -11,27 +11,36 @@ type handlerSignature = (
   res: NextApiResponse<HttpResponse>
 ) => void;
 
+const { DB_AUTH_TOKEN } = process.env;
+
 /**
  * Imitating database.
  */
 const handler: handlerSignature = async (req, res) => {
-  switch (req.method) {
-    case "GET":
-      res.status(200).send(new HttpResponse(200, [...USERS]));
-    case "PUT": {
-      const { firstName, secondName, email, password, id } =
-        req.body as FullUser;
+  const authToken = undefined;
 
-      if (USERS.some(user => user.email === email)) {
-        res.status(500).send(new HttpResponse(500));
-      } else {
-        USERS.push({ firstName, secondName, email, password, id });
-        res.status(201).send(new HttpResponse(201));
+  const authTokenTrimmed: string | undefined =
+    authToken && req.headers.authorization?.slice(7);
+
+  if (authTokenTrimmed === DB_AUTH_TOKEN) {
+    switch (req.method) {
+      case "GET":
+        res.status(200).send(new HttpResponse(200, [...USERS]));
+      case "PUT": {
+        const { firstName, secondName, email, password, id } =
+          req.body as FullUser;
+
+        if (USERS.some(user => user.email === email)) {
+          res.status(500).send(new HttpResponse(500));
+        } else {
+          USERS.push({ firstName, secondName, email, password, id });
+          res.status(201).send(new HttpResponse(201));
+        }
       }
+      default:
+        res.status(500).send(new HttpResponse(500));
     }
-    default:
-      res.status(500).send(new HttpResponse(500));
-  }
+  } else res.status(500).send(new HttpResponse(500));
 };
 
 export default handler;
