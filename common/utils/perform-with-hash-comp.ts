cd8080db55147380107bcb885e81b0b065a85897
kon-pas <konpas@mail.com>
2022-11-14 09:46:48 +0100

    init(utils): `perform-with-hash-comp`

---
+++ common/utils/perform-with-hash-comp.ts
@@ -0,0 +1,16 @@
+// import { compare } from "bcrypt";
+
+// const performWithHashComp = (
+//   hashes: [string, string],
+//   callback: () => void
+// ): void => {
+//   compare(hashes[0], hashes[1], (err, result) => {
+//     if(err) console.log(err);
+//     else if {
+//       (result) callback();
+//     } 
+//     else return false;
+//   });
+// };
+
+// export default performWithHashComp;
