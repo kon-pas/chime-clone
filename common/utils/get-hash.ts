72f92633ea0c6c8f168482b60d20633c1c21bef9
kon-pas <konpas@mail.com>
2022-11-14 09:46:26 +0100

    init(utils): `get-hash`

---
+++ common/utils/get-hash.ts
@@ -0,0 +1,17 @@
+import { hash, genSalt } from "bcrypt";
+
+const { HASH_BASE_SALT } = process.env;
+
+/**
+ * Returns hashed value.
+ * Use only server-side.
+ * 
+ * @param value - string to hash
+ * @returns `Promise` resolving a hashed string
+ */
+const getHash = async (value: string): Promise<string> => {
+  const salt = await genSalt(Number(HASH_BASE_SALT));
+  return await hash(value, salt);
+};
+
+export default getHash;
