ec8017a5cdb80730ac606fb0b84d0b9663079b56
kon-pas <konpas@mail.com>
2022-11-30 22:52:24 +0100

    feat: handle `styled-components` server-side

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,23 +1,53 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { ServerStyleSheet } from "styled-components";
+import Document, {
+  DocumentContext,
+  Html,
+  Head,
+  Main,
+  NextScript,
+} from "next/document";
 
-export default function Document() {
-  return (
-    <Html>
-      <Head>
-        <link
-          rel="icon"
-          href="https://www.chime.com/wp-content/uploads/2018/03/favicon.png"
-        />
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
-        />
-      </Head>
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
 
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+
+      return {
+        ...initialProps,
+        styles: [initialProps.styles, sheet.getStyleElement()],
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            rel="icon"
+            href="https://www.chime.com/wp-content/uploads/2018/03/favicon.png"
+          />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
+          />
+        </Head>
+
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
