03b98e3e0f921c7f86c284d2b593ab04bbaed53b
kon-pas <konpas@mail.com>
2022-12-02 22:06:44 +0100

    add: favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -33,14 +33,8 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link
-            rel="icon"
-            href="https://www.chime.com/wp-content/uploads/2018/03/favicon.png"
-          />
-          <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
-          />
+          <link rel="icon" href="/static/shared/favicon.png" sizes="32x32" />
+          <link rel="icon" href="/static/shared/favicon.png" sizes="192x192" />
         </Head>
 
         <body>
