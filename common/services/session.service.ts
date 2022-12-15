e345ea45921eb769120ce97e67d64220edec3398
kon-pas <konpas@mail.com>
2022-12-15 04:49:22 +0100

    wip(services): `session`

--- common/services/session.service.ts
+++ common/services/session.service.ts
@@ -1,36 +1,38 @@
 import type { SafeUser } from "@interfaces";
 import type { Email, FirstName, SecondName } from "@types";
 
+const { setItem, getItem } = sessionStorage;
+
 const user = {
   logIn(user: SafeUser): void {
-    window.sessionStorage.setItem("user", JSON.stringify(user));
+    setItem("user", JSON.stringify(user));
   },
 
   logOut(): void {
-    window.sessionStorage.setItem("user", "");
+    setItem("user", "");
   },
 
   isLogged(): boolean {
-    return window.sessionStorage.getItem("user") === null;
+    return getItem("user") === null;
   },
 
   getSafeData(): SafeUser | null {
-    const data = window.sessionStorage.getItem("user");
+    const data = getItem("user");
     return data ? JSON.parse(data) : null;
   },
 
   getEmail(): Email | null {
-    const data = window.sessionStorage.getItem("user");
+    const data = getItem("user");
     return data ? JSON.parse(data).email : null;
   },
 
   getFirstName(): FirstName | null {
-    const data = window.sessionStorage.getItem("user");
+    const data = getItem("user");
     return data ? JSON.parse(data).firstName : null;
   },
 
   getSecondName(): SecondName | null {
-    const data = window.sessionStorage.getItem("user");
+    const data = getItem("user");
     return data ? JSON.parse(data).secondName : null;
   },
 };
@@ -38,4 +40,3 @@ const user = {
 const session = { user };
 
 export default session;
- 
