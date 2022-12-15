d7a9515a5b0da62bef3a9eabacb4dc28fee7c7ec
kon-pas <konpas@mail.com>
2022-12-15 22:12:47 +0100

    add(`BannerSection`): `align: cover` option

--- common/components/sections/BannerSection/styled/Wrapper.styled.ts
+++ common/components/sections/BannerSection/styled/Wrapper.styled.ts
@@ -6,12 +6,20 @@ import { COLORS } from "@constants";
 
 interface WrapperProps {
   backgroundColor?: HexColor;
+  src?: `/static/${string}`;
+  coverBreakpoint?: number;
 }
 
 const Wrapper = styled.div<WrapperProps>`
   background-color: ${props => props.backgroundColor ?? COLORS.WHITE};
   width: 100%;
+  height: 100%;
   padding: 2rem 0;
+  background: url("/static/pages/home/web-01-app-desktop.png") no-repeat
+    ${props => (props.coverBreakpoint ? `${props.coverBreakpoint}%` : "center")}
+    center;
+  background-size: cover;
+  overflow: hidden;
 `;
 
 export default Wrapper;
