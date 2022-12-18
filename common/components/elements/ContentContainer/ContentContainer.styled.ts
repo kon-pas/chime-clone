93123866e128733cdf0a1098f101070d9d911734
kon-pas <konpas@mail.com>
2022-12-18 23:55:47 +0100

    refactor(`ContentContainer`): use `content` mixin

--- common/components/elements/ContentContainer/ContentContainer.styled.ts
+++ common/components/elements/ContentContainer/ContentContainer.styled.ts
@@ -1,23 +1,9 @@
 import styled from "styled-components";
 
-import { BREAKPOINTS } from "@constants";
-
-import { getMedia, getResizeTransition } from "@utils";
+import { container } from "@styles/Mixins.styled";
 
 const ContentContainer = styled.div`
-  padding: 0 1.4rem;
-
-  ${getResizeTransition("width", "padding", "margin")}
-
-  ${getMedia("md")} {
-    width: ${BREAKPOINTS.MD}px;
-    margin: 0 auto;
-  }
-
-  ${getMedia("lg")} {
-    width: ${BREAKPOINTS.LG}px;
-    padding: 0 2rem;
-  }
+  ${container};
 `;
 
 export default ContentContainer;
