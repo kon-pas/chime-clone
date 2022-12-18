d0625870fb1c415ee7a07fe00f683c525a12cfba
kon-pas <konpas@mail.com>
2022-12-19 00:45:51 +0100

    add(`Mixins): `button`

--- common/styles/Mixins.styled.ts
+++ common/styles/Mixins.styled.ts
@@ -1,6 +1,6 @@
 import { css } from "styled-components";
 
-import { BREAKPOINTS } from "@constants";
+import { COLORS, BREAKPOINTS } from "@constants";
 import { getMedia, getResizeTransition } from "@utils";
 
 export const container = css`
@@ -16,5 +16,27 @@ export const container = css`
   ${getMedia("lg")} {
     width: ${BREAKPOINTS.LG}px;
     padding: 0 2rem;
+  }
+`;
+
+export const button = css`
+  border: 1px solid ${COLORS.BLACK};
+  border-radius: 10rem;
+  text-align: center;
+  white-space: nowrap;
+  font-size: 1rem;
+  background: none;
+  padding: 0.7rem 1.3rem;
+  cursor: pointer;
+  transition: color 0.2s linear, background-color 0.2s linear;
+
+  &:hover {
+    background-color: ${COLORS.BLACK};
+    color: ${COLORS.WHITE};
+  }
+
+  svg {
+    margin-left: 3px;
+    width: 10px;
   }
 `;
