cf4422e4c3a8d617059efe1e00b9e2a4aa420870
kon-pas <konpas@mail.com>
2022-12-19 04:33:24 +0100

    add(`ToggleSectionWrapper`): toggle between views

--- common/components/elements/ToggleSectionWrapper/index.tsx
+++ common/components/elements/ToggleSectionWrapper/index.tsx
@@ -6,27 +6,22 @@ import { useEffect, useState } from "react";
 
 interface ToggleSectionWrapperProps {
   views: ReactNode[];
-  minHeight?: number;
 }
 
 const ToggleSectionWrapper: FC<ToggleSectionWrapperProps> = props => {
-  const [viewIdx, setViewIdx] = useState<number>(3);
+  const [viewIdx, setViewIdx] = useState<number>(0);
 
-  // useEffect(() => {
-  //   const intervalDescriptor: NodeJS.Timer = setInterval(() => {
-  //     setViewIdx(current => (current + 1) % props.views.length);
-  //   }, 3000);
-  //   return () => clearInterval(intervalDescriptor);
-  // }, [props.views.length]);
+  useEffect(() => {
+    const intervalDescriptor: NodeJS.Timer = setInterval(() => {
+      setViewIdx(current => (current + 1) % props.views.length);
+    }, 3000);
+    return () => clearInterval(intervalDescriptor);
+  }, [props.views.length]);
 
   return (
     <Styled.Container>
       {props.views.map((view, idx) => (
-        <Styled.View
-          key={idx}
-          visible={idx === viewIdx}
-          minHeight={props.minHeight}
-        >
+        <Styled.View key={idx} visible={idx === viewIdx}>
           {view}
         </Styled.View>
       ))}
