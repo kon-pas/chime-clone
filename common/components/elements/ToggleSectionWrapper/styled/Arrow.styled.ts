332030b3595c1e6eb7abbc4f70a65c58570b0c92
kon-pas <konpas@mail.com>
2022-12-19 23:50:18 +0100

    style(`ToggleSectionWrapper`): `Arrow`

--- common/components/elements/ToggleSectionWrapper/styled/Arrow.styled.ts
+++ common/components/elements/ToggleSectionWrapper/styled/Arrow.styled.ts
@@ -1,9 +1,31 @@
 import styled from "styled-components";
 
+import { getMedia } from "@utils";
+import { Breakpoints } from "@enums";
+
 interface ArrowProps {
   side: "left" | "right";
 }
 
-const Arrow = styled.div``;
+const Arrow = styled.div<ArrowProps>`
+  position: absolute;
+  display: none;
+  ${props => (props.side === "left" ? `left: 10px;` : `right: 10px;`)}
+  top: 50%;
+  transform: translateY(-50%);
+  width: 35px;
+
+  @media (min-width: ${Breakpoints.md + 50}px) {
+    display: block;
+  }
+
+  @media (min-width: ${Breakpoints.lg}px) {
+    display: none;
+  }
+
+  @media (min-width: ${Breakpoints.lg + 50}px) {
+    display: block;
+  }
+`;
 
 export default Arrow;
