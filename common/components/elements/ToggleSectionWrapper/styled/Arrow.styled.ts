396ef6d1c802540f8b8b0881ac7ce3eeedaaa017
kon-pas <konpas@mail.com>
2022-12-19 23:57:41 +0100

    style(`Arrow`): add hover effect

--- common/components/elements/ToggleSectionWrapper/styled/Arrow.styled.ts
+++ common/components/elements/ToggleSectionWrapper/styled/Arrow.styled.ts
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 import { getMedia } from "@utils";
 import { Breakpoints } from "@enums";
+import { COLORS } from "@constants";
 
 interface ArrowProps {
   side: "left" | "right";
@@ -14,6 +15,16 @@ const Arrow = styled.div<ArrowProps>`
   top: 50%;
   transform: translateY(-50%);
   width: 35px;
+  color: ${COLORS.GRAY_MEDIUM};
+
+  opacity: 0.8;
+  transition-property: opacity;
+  transition-duration: 0.1s;
+
+  &:hover {
+    cursor: pointer;
+    opacity: 1;
+  }
 
   @media (min-width: ${Breakpoints.md + 50}px) {
     display: block;
