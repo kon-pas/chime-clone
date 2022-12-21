80bb1c496a7bfa0282f3b862ed283c819081d26a
kon-pas <konpas@mail.com>
2022-12-21 22:50:11 +0100

    feat(`api`): convert to `prisma`

--- pages/api/users/log-in.ts
+++ pages/api/users/log-in.ts
@@ -2,53 +2,39 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import type { FullUser } from "@interfaces";
 
 import { compare } from "bcrypt";
-import { HttpResponse, fetchWrapper } from "@api";
-
-const { DB_HOST_FAKE, DB_AUTH_TOKEN_FAKE } = process.env;
+import { HttpResponse } from "@api";
+import { prisma } from "@lib";
 
 const handler = async (
   req: NextApiRequest,
   res: NextApiResponse<HttpResponse>
 ): Promise<void> => {
+
   try {
-    const response: HttpResponse = await fetchWrapper
-      .get({
-        url: `${DB_HOST_FAKE}/users`,
-        auth: DB_AUTH_TOKEN_FAKE,
-      })
-      .then(res => res.json());
+    const users: FullUser[] = await prisma.user.findMany({});
 
     if (req.method === "POST") {
       const { email: targetEmail, password: targetPassword } = req.body;
 
-      if (response.success) {
-        const users = response.body as FullUser[];
+      const foundUser: FullUser | undefined = users.find(
+        (user: FullUser) => user.email === targetEmail
+      );
 
-        const foundUser: FullUser | undefined = users.find(
-          (user: FullUser) => user.email === targetEmail
-        );
-
-        if (foundUser) {
-          if (await compare(targetPassword, foundUser.password))
-            res.status(200).send(
-              new HttpResponse(200, {
-                firstName: foundUser.firstName,
-                secondName: foundUser.secondName,
-                email: foundUser.email,
-              })
-            );
-          else
-            res
-              .status(404)
-              .send(new HttpResponse(404, { msg: "Incorrect Password" }));
-        } else
+      if (foundUser) {
+        if (await compare(targetPassword, foundUser.password))
+          res.status(200).send(
+            new HttpResponse(200, {
+              firstName: foundUser.firstName,
+              secondName: foundUser.secondName,
+              email: foundUser.email,
+            })
+          );
+        else
           res
             .status(404)
-            .send(new HttpResponse(404, { msg: "User Not Found" }));
+            .send(new HttpResponse(404, { msg: "Incorrect Password" }));
       } else
-        res
-          .status(response.statusCode)
-          .send(new HttpResponse(response.statusCode));
+        res.status(404).send(new HttpResponse(404, { msg: "User Not Found" }));
     }
   } catch (error) {
     res.status(500).send(new HttpResponse(500));
