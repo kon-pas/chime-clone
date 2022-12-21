20c886de8ed7c596661b37c79c424e93c8ee5c2e
kon-pas <konpas@mail.com>
2022-12-21 22:50:47 +0100

    refactor(`api`): improved control flow

--- pages/api/users/log-in.ts
+++ pages/api/users/log-in.ts
@@ -9,35 +9,45 @@ const handler = async (
   req: NextApiRequest,
   res: NextApiResponse<HttpResponse>
 ): Promise<void> => {
+  switch (req.method) {
+    case "POST": {
+      try {
+        const { email: targetEmail, password: targetPassword } = req.body;
 
-  try {
-    const users: FullUser[] = await prisma.user.findMany({});
+        const foundUser: FullUser | null = await prisma.user.findUnique({
+          where: {
+            email: targetEmail,
+          },
+        });
 
-    if (req.method === "POST") {
-      const { email: targetEmail, password: targetPassword } = req.body;
-
-      const foundUser: FullUser | undefined = users.find(
-        (user: FullUser) => user.email === targetEmail
-      );
-
-      if (foundUser) {
-        if (await compare(targetPassword, foundUser.password))
-          res.status(200).send(
-            new HttpResponse(200, {
-              firstName: foundUser.firstName,
-              secondName: foundUser.secondName,
-              email: foundUser.email,
-            })
-          );
-        else
+        if (foundUser) {
+          if (await compare(targetPassword, foundUser.password))
+            res.status(200).send(
+              new HttpResponse(200, {
+                firstName: foundUser.firstName,
+                secondName: foundUser.secondName,
+                email: foundUser.email,
+              })
+            );
+          else
+            res
+              .status(404)
+              .send(new HttpResponse(404, { msg: "Incorrect Password" }));
+        } else {
           res
             .status(404)
-            .send(new HttpResponse(404, { msg: "Incorrect Password" }));
-      } else
-        res.status(404).send(new HttpResponse(404, { msg: "User Not Found" }));
+            .send(new HttpResponse(404, { msg: "User Not Found" }));
+        }
+      } catch {
+        res.status(500).send(new HttpResponse(500));
+      } finally {
+        break;
+      }
     }
-  } catch (error) {
-    res.status(500).send(new HttpResponse(500));
+    default: {
+      res.status(500).send(new HttpResponse(500));
+      break;
+    }
   }
 };
 
