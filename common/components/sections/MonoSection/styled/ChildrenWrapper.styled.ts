bfe0462df2e25817984a5612f21d19707ed0e227
kon-pas <konpas@mail.com>
2022-12-24 16:53:40 +0100

    style(`MonoSection`): fix specificity

--- common/components/sections/MonoSection/styled/ChildrenWrapper.styled.ts
+++ common/components/sections/MonoSection/styled/ChildrenWrapper.styled.ts
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 import { getMedia, getResizeTransition } from "@utils";
 
-import { h2, p } from "@styles/Tags.styled";
+import { h2, p, ul } from "@styles/Tags.styled";
 
 interface ChildrenWrapperProps {
   textAlign: "start" | "center" | "end";
@@ -13,12 +13,17 @@ const ChildrenWrapper = styled.div<ChildrenWrapperProps>`
   display: grid;
   gap: 2rem;
 
-  p {
+  > p {
     ${p}
+    width: 100%;
   }
 
-  h2 {
+  > h2 {
     ${h2}
+  }
+
+  > ul {
+    ${ul}
   }
 
   ${getResizeTransition("width", "margin")}
