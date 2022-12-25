b7b627556ada23ebc8e115f00a6883392df13a50
kon-pas <konpas@mail.com>
2022-12-25 23:41:12 +0100

    init(`pages`): `/chime-financial`

--- common/components/pages/index.ts
+++ common/components/pages/index.ts
@@ -9,3 +9,5 @@ export * as LogInComponents from "./log-in";
 export * as SignUpComponents from "./sign-up";
 
 export * as SecurityAndControlComponents from "./security-and-control";
+
+export * as ChimeFinancialComponents from "./chime-financial";
