587785f87d06bae6f12a96f3e8e3403fec0dd729
kon-pas <konpas@mail.com>
2022-12-28 23:25:41 +0100

    init(`/chime-financial`): `Section`

--- common/components/pages/chime-financial/Section/Container.styled.ts
+++ common/components/pages/chime-financial/Section/Container.styled.ts
@@ -1,7 +1,16 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
 import { ContentContainer } from "@components/elements";
 
-const Container = styled(ContentContainer)``
+import { getMedia } from "@utils";
+
+const Container = styled(ContentContainer)`
+  padding-top: 2rem;
+  padding-bottom: 2rem;
+
+  ${getMedia("md")} {
+    
+  }
+`;
 
 export default Container;
--- common/components/pages/chime-financial/Section/Title.styled.ts
+++ common/components/pages/chime-financial/Section/Title.styled.ts
@@ -1,5 +1,22 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
-const Title = styled.h2``
+import { COLORS } from "@constants";
+
+import { getMedia, getResizeTransition } from "@utils";
+
+const Title = styled.h2`
+  font-size: 1.9rem;
+  line-height: 2.65rem;
+  letter-spacing: 0.005em;
+
+  ${getResizeTransition("font-size")}
+
+  ${getMedia("lg")} {
+    font-size: 2.1rem;
+  }
+
+  border-bottom: 1px solid ${COLORS.GRAY_MEDIUM};
+  padding-bottom: 2rem;
+`;
 
 export default Title;
