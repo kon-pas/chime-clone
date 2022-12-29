a89c4a4eecffcf281eae795d7834096f9e522ebd
kon-pas <konpas@mail.com>
2022-12-29 06:07:46 +0100

    chore(`styles`): export `tags`

---
+++ common/styles/tags/index.ts
@@ -0,0 +1,15 @@
+export { default as button } from "./button.styled";
+
+export { default as container } from "./container.styled";
+
+export { default as h1 } from "./h1.styled";
+
+export { default as h2 } from "./h2.styled";
+
+export { default as h3 } from "./h3.styled";
+
+export { default as li } from "./li.styled";
+
+export { default as p } from "./p.styled";
+
+export { default as ul } from "./ul.styled";
