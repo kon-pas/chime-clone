09f9b72fa94e2236dafee5fc86f885af193bd50c
kon-pas <konpas@mail.com>
2022-12-29 06:09:59 +0100

    style(`/chime-financial`): `Section`

--- common/components/pages/chime-financial/Section/Container.styled.ts
+++ common/components/pages/chime-financial/Section/Container.styled.ts
@@ -1,7 +1,5 @@
 import styled from "styled-components";
-
 import { ContentContainer } from "@components/elements";
-
 import { getMedia } from "@utils";
 
 const Container = styled(ContentContainer)`
@@ -9,7 +7,14 @@ const Container = styled(ContentContainer)`
   padding-bottom: 2rem;
 
   ${getMedia("md")} {
-    
+    display: grid;
+    grid-template-columns: 20% 1fr;
+    gap: 5rem;
+  }
+
+  ${getMedia("lg")} {
+    grid-template-columns: 25% 1fr;
+    gap: 5rem;
   }
 `;
 
--- common/components/pages/chime-financial/Section/Title.styled.ts
+++ common/components/pages/chime-financial/Section/Title.styled.ts
@@ -1,22 +1,25 @@
 import styled from "styled-components";
-
 import { COLORS } from "@constants";
-
 import { getMedia, getResizeTransition } from "@utils";
 
 const Title = styled.h2`
   font-size: 1.9rem;
   line-height: 2.65rem;
   letter-spacing: 0.005em;
+  border-bottom: 1px solid ${COLORS.GRAY_MEDIUM};
+  padding-bottom: 2rem;
 
-  ${getResizeTransition("font-size")}
+  ${getResizeTransition("font-size", "margin-top")}
+
+  ${getMedia("md")} {
+    border-bottom: none;
+    margin-top: 1.7rem;
+  }
 
   ${getMedia("lg")} {
     font-size: 2.1rem;
+    margin-top: 1.8rem;
   }
-
-  border-bottom: 1px solid ${COLORS.GRAY_MEDIUM};
-  padding-bottom: 2rem;
 `;
 
 export default Title;
